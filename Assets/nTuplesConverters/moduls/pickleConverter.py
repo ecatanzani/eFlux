0d87d3599759c7a78bb432158d9d793a3819b5e3
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2019-12-30 18:08:20 +0100

    picke converter module
    module to convert pickle numpy files to TTree files

---
+++ Assets/nTuplesConverters/moduls/pickleConverter.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import numpy as np
+from ROOT import TTree, TFile, TBranch
+
+def convertPickle(opts):
+    outFile = TFile(opts.output,"RECREATE")
+    if outFile.IsZombie():
+        print('Error writing output file {}'.format(opts.output))
+        sys.exit()
+    myTree = TTree("collectionTree","All Particle Tree")
+
+    energy = []
+    xtrl = []
+    
+    for file in os.listdir(opts.input):
+        fNpath = str(opts.input) + "/" + str(file)
+        if opts.verbose:
+            print('Converting {}'.format(fNpath))
+        data = np.load(fNpath,'r')
+        nRows = np.size(data,0)
+        for evIdx in range(0,nRows):
+            energy.append(data[evIdx,44]/1000.)
+            xtrl.append(data[evIdx,49])
+    
+    t_energy = np.asarray(energy)
+    t_xtrl = np.asarray(xtrl)
+    nevents = len(energy)
+
+    del energy
+    del xtrl
+
+    myTree.Branch( 'eReco', t_energy, 'eReco[nevents]/F' )
+    myTree.Branch( 'xtrl', d, 'xtrl[nevents]/F' )
+
+    myTree.Fill()
+
+    outFile.Write()
+    outFile.Close()
