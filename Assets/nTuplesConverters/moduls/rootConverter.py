62bca41c5ffd461869905d4183e9af1aa3b6af59
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-06 14:26:51 +0100

    Updated ROOT moudule with TChain and dedicated TTree moduls
    Input data are threated using a TChain.
    TTree data management has been done using specific moduls

--- Assets/nTuplesConverters/moduls/rootConverter.py
+++ Assets/nTuplesConverters/moduls/rootConverter.py
@@ -1,40 +1,81 @@
 import os
 import sys
-import numpy as np
-from ROOT import TTree, TFile, TBranch
+from array import array
+from ROOT import TChain,TTree,TFile,TBranch
 
 def convertROOT(opts):
+    
+    # Import TTree branching module
+    from handleTree import branchTree,fillTreeChain
+
+    # Create output TFile
     outFile = TFile(opts.output,"RECREATE")
     if outFile.IsZombie():
         print('Error writing output file {}'.format(opts.output))
         sys.exit()
-    myTree = TTree("collectionTree","All Particle Tree")
 
-    energy = []
-    xtrl = []
-    
+    # Create TChain to handle input ROOT files - "DmlNtup" is the TTree name in each ntuple ROOT file
+    dChain = TChain("DmlNtup")
+
+    # Add ROOT files to the chain
     for file in os.listdir(opts.input):
         fNpath = str(opts.input) + "/" + str(file)
         if opts.verbose:
-            print('Converting {}'.format(fNpath))
-        
-
-        data = TFile.Open(fNpath)
-        for event in data.DmlNtup :
-            energy.append(event.tt_bgoTotalEcorr_GeV/1000.)
-            xtrl.append(event.tt_Xtrl)
+            print('Adding {} to the chain...'.format(fNpath))
+        dChain.AddFile(fNpath)
     
-    t_energy = np.asarray(energy)
-    t_xtrl = np.asarray(xtrl)
-    nevents = len(energy)
+    nevents = dChain.GetEntries()
+    if opts.verbose:
+        print('Collected {} events ...'.format(nevents))
+    
+    # Create output TTree
+    myTree = TTree("collectionTree","All Particle Tree")
+    
+    # Create final arrays for the TTree
+    t_totalEnergy = array('f',[0.])
+    t_totalEnergyCorr = array('f',[0.])
+    t_xtrl = array('f',[0.])
+    t_satPositionX = array('f',[0.])
+    t_satPositionY = array('f',[0.])
+    t_satPositionZ = array('f',[0.])
+    t_satVelocityX = array('f',[0.])
+    t_satVelocityY = array('f',[0.])
+    t_satVelocityZ = array('f',[0.])
 
-    del energy
-    del xtrl
+    # Branch TTree
+    branchTree(
+                opts,
+                myTree,
+                t_totalEnergyCorr,
+                t_totalEnergy,
+                t_xtrl,
+                t_satPositionX,
+                t_satPositionY,
+                t_satPositionZ,
+                t_satVelocityX,
+                t_satVelocityY,
+                t_satVelocityZ
+                )
 
-    myTree.Branch( 'eReco', t_energy, 'eReco/F' )
-    myTree.Branch( 'xtrl', t_xtrl, 'xtrl/F' )
+    # Filling the TTree
+    fillTreeChain(
+                opts,
+                dChain,
+                nevents,
+                myTree,
+                t_totalEnergyCorr,
+                t_totalEnergy,
+                t_xtrl,
+                t_satPositionX,
+                t_satPositionY,
+                t_satPositionZ,
+                t_satVelocityX,
+                t_satVelocityY,
+                t_satVelocityZ,
+                kStep=1e+4
+                )
 
-    myTree.Fill()
-
+    # Writing optput file
     outFile.Write()
     outFile.Close()
+   
