e47825c0c0d43094c07cb61a6a6128ed629e9466
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-06 14:27:53 +0100

    Updated with specific TTree moduls

--- Assets/nTuplesConverters/moduls/pickleConverter.py
+++ Assets/nTuplesConverters/moduls/pickleConverter.py
@@ -4,36 +4,60 @@ import numpy as np
 from ROOT import TTree, TFile, TBranch
 
 def convertPickle(opts):
+
+    # Import TTree branching module
+    from handleTree import branchTree,fillTree
+
+    # Create output TFile
     outFile = TFile(opts.output,"RECREATE")
     if outFile.IsZombie():
         print('Error writing output file {}'.format(opts.output))
         sys.exit()
+    
+    # Create output TTree
     myTree = TTree("collectionTree","All Particle Tree")
 
-    energy = []
-    xtrl = []
-    
-    for file in os.listdir(opts.input):
-        fNpath = str(opts.input) + "/" + str(file)
-        if opts.verbose:
-            print('Converting {}'.format(fNpath))
-        data = np.load(fNpath,'r')
-        nRows = np.size(data,0)
-        for evIdx in range(0,nRows):
-            energy.append(data[evIdx,44]/1000.)
-            xtrl.append(data[evIdx,49])
-    
-    t_energy = np.asarray(energy)
-    t_xtrl = np.asarray(xtrl)
-    nevents = len(energy)
+    # Create final arrays for the TTree
+    t_totalEnergy = array('f',[0.])
+    t_totalEnergyCorr = array('f',[0.])
+    t_xtrl = array('f',[0.])
+    t_satPositionX = array('f',[0.])
+    t_satPositionY = array('f',[0.])
+    t_satPositionZ = array('f',[0.])
+    t_satVelocityX = array('f',[0.])
+    t_satVelocityY = array('f',[0.])
+    t_satVelocityZ = array('f',[0.])
 
-    del energy
-    del xtrl
+    # Branch TTree
+    branchTree(
+                opts,
+                myTree,
+                t_totalEnergyCorr,
+                t_totalEnergy,
+                t_xtrl,
+                t_satPositionX,
+                t_satPositionY,
+                t_satPositionZ,
+                t_satVelocityX,
+                t_satVelocityY,
+                t_satVelocityZ
+                )
 
-    myTree.Branch( 'eReco', t_energy, 'eReco/F' )
-    myTree.Branch( 'xtrl', t_xtrl, 'xtrl/F' )
+    # Filling the TTree
+    fillTree(
+                opts,
+                myTree,
+                t_totalEnergyCorr,
+                t_totalEnergy,
+                t_xtrl,
+                t_satPositionX,
+                t_satPositionY,
+                t_satPositionZ,
+                t_satVelocityX,
+                t_satVelocityY,
+                t_satVelocityZ
+                )
 
-    myTree.Fill()
-
+    # Writing optput file
     outFile.Write()
     outFile.Close()
