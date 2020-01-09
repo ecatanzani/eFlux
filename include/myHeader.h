f204c23aeeed2796c36a51f7ee2984f8319e3f12
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-09 22:48:17 +0100

    Analysis functions included

--- include/myHeader.h
+++ include/myHeader.h
@@ -4,6 +4,20 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <vector>
 
+#include "TTree.h"
+
+#pragma once
+
+extern void eCore(
+                    const std::string inputPath,
+                    const std::string outputPath,
+                    const bool verbose
+                );
+
+extern void readInputTree(const std::string inputPath,std::vector<double> &dataValues);
+extern void branchTree(TTree &myDataTree,std::vector<double> &dataValues);
 
 #endif
