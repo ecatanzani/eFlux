f5e7a22d8f855159e3d32c166e828ad81ccef7fc
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-09 22:48:35 +0100

    Input TTree import and branching

--- source/importTree.cpp
+++ source/importTree.cpp
@@ -1,6 +1,32 @@
 #include "myHeader.h"
 
-void exists()
+#include "TFile.h"
+#include "TTree.h"
+
+#include <vector>
+
+void readInputTree(const std::string inputPath,std::vector<double> &dataValues)
 {
-    std::cout << "Sure I Exists !!";
+    TFile inputTree(inputPath.c_str(),"READ");
+    if(!inputTree.IsOpen())
+    {
+        std::cerr << "\n\nError opening input TTree: " << inputPath;
+        exit(123);
+    }
+    TTree *myDataTree = nullptr;
+    inputTree.GetObject("collectionTree",myDataTree);
+    branchTree(*myDataTree,dataValues);
+}
+
+void branchTree(TTree &myDataTree,std::vector<double> &dataValues)
+{
+    myDataTree.SetBranchAddress("eReco",&dataValues[0]);
+    myDataTree.SetBranchAddress("eRecoCorr",&dataValues[1]);
+    myDataTree.SetBranchAddress("xtrl",&dataValues[2]);
+    myDataTree.SetBranchAddress("posizionX",&dataValues[3]);
+    myDataTree.SetBranchAddress("posizionY",&dataValues[4]);
+    myDataTree.SetBranchAddress("posizionZ",&dataValues[5]);
+    myDataTree.SetBranchAddress("velocityX",&dataValues[6]);
+    myDataTree.SetBranchAddress("velocityY",&dataValues[7]);
+    myDataTree.SetBranchAddress("velocityZ",&dataValues[8]);
 }
