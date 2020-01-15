cd6236edd7399fd087e59dadb82a2adfdc2d2444
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-15 22:22:57 +0100

    Core is now dedicated to the flux building

--- source/Core.cpp
+++ source/Core.cpp
@@ -1,39 +1,23 @@
 #include "myHeader.h"
 
-#include "TTree.h"
-
-#include <vector>
-
 void eCore(
             const std::string inputPath,
             const std::string outputPath,
             const bool verbose
             )
 {
-    /* TTree variables
-
-    double totalEnergy = 0;               vector position 0
-    double totalEnergyCorr = 0;           vector position 1
-    double xtrl = 0;                      vector position 2
-    double satPositionX = 0;              vector position 3
-    double satPositionY = 0;              vector position 4
-    double satPositionZ = 0;              vector position 5
-    double satVelocityX = 0;              vector position 6
-    double satVelocityY = 0;              vector position 7
-    double satVelocityZ = 0;              vector position 8
-
-    */
-
-    unsigned nData = 9;
-    std::vector<double> dataValues(nData,0);
-
-    TTree* dTree = new TTree("collectionTree","Data Collection Tree");
-
-    readInputTree(inputPath,dataValues,dTree);
+    //Create output TFile
+    TFile outFile(uniqueTree(outputPath),"NEW","Analysis Output TTree");
+    if(!outFile.IsOpen())
+    {
+        std::cerr << "\n\nError writing output TFile: " << uniqueTree(outputPath) << std::endl;
+        exit(123);
+    }
     
+    //Building eFLux
+    buildFlux(outFile);
 
-    //Cleanup memory ...
-
-    dTree->Delete();
+    //Close output file ...
+    outFile.Close();
 
 }
