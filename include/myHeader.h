b6ad93cc38a8b31da008062bde6dc2b262162112
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-15 22:22:29 +0100

    Modified old analysis functions and new one have been added
    Modified general scheme of the project. Now event loop has a dedicate function

--- include/myHeader.h
+++ include/myHeader.h
@@ -8,6 +8,8 @@
 #include <vector>
 
 #include "TTree.h"
+#include "TFile.h"
+#include "TH2D.h"
 
 #pragma once
 
@@ -19,5 +21,11 @@ extern void eCore(
 
 extern void readInputTree(const std::string inputPath,std::vector<double> &dataValues,TTree* dTree);
 extern void branchTree(TTree &myDataTree,std::vector<double> &dataValues);
+extern const char* uniqueTree(const std::string outputPath);
+extern void uniqueTree(TTree *dTree,const TFile &outFile);
+extern std::vector<double> createLogBinning(const double minValue,const double maxValue,const int nBins);
+extern void buildFlux(TFile &outFile);
+extern void buildXtrlFlux(TFile &outFile,std::vector<double> &logBins);
+extern void evLoop(TH2D &inputHisto,const std::string inputPath,const bool eClassifier);
 
 #endif
