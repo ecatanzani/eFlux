84394b2aa3dbb2c3227ad9e7bb06978c4ff8ca1f
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-17 10:24:19 +0100

    Completed the event loop function
    Also included telemetry information to final out file

--- source/eventLoop.cpp
+++ source/eventLoop.cpp
@@ -1,19 +1,28 @@
 #include "myHeader.h"
 
-void evLoop(TH2D &inputHisto,const std::string inputPath,const bool eClassifier)
+void evLoop(
+                    TFile &outFile,
+                    TH1D &inputHisto,
+                    const std::string inputPath,
+                    const bool eClassifier,
+                    const double xtrlCut
+                )
 {
 
     /* TTree variables
 
-    double totalEnergy = 0;               vector position 0
-    double totalEnergyCorr = 0;           vector position 1
-    double xtrl = 0;                      vector position 2
-    double satPositionX = 0;              vector position 3
-    double satPositionY = 0;              vector position 4
-    double satPositionZ = 0;              vector position 5
-    double satVelocityX = 0;              vector position 6
-    double satVelocityY = 0;              vector position 7
-    double satVelocityZ = 0;              vector position 8
+    double totalEnergy;               vector position 0
+    double totalEnergyCorr;           vector position 1
+    double xtrl;                      vector position 2
+    double satPositionX;              vector position 3
+    double satPositionY;              vector position 4
+    double satPositionZ;              vector position 5
+    double satVelocityX;              vector position 6
+    double satVelocityY;              vector position 7
+    double satVelocityZ;              vector position 8
+
+    eClassifier True -> XTRL
+    eClassifier False -> Manual Cuts
 
     */
 
@@ -25,8 +34,49 @@ void evLoop(TH2D &inputHisto,const std::string inputPath,const bool eClassifier)
 
     //Linking input TTree
     readInputTree(inputPath,dataValues,dTree);
-   
+    
+    /*
+        Telemetry information histos
+            - Position of the satellite
+            - Velocity of the satellite
+    */
+
+    TH1D satellitePosX("satellitePosX","satellite position X",1e+6,-8e+6,8e+6);
+    TH1D satellitePosY("satellitePosY","satellite position Y",1e+6,-8e+6,8e+6);
+    TH1D satellitePosZ("satellitePosZ","satellite position Z",1e+6,-8e+6,8e+6);
+
+    TH1D satelliteVelX("satelliteVelX","satellite velocity X",1e+6,-8e+3,8e+3);
+    TH1D satelliteVelY("satelliteVelY","satellite velocity Y",1e+6,-8e+3,8e+3);
+    TH1D satelliteVelZ("satelliteVelZ","satellite velocity Z",1e+6,-8e+3,8e+3);
+
+
+    for(unsigned int evIdx=0; evIdx<dTree->GetEntries(); ++evIdx)
+    {
+        dTree->GetEntry(evIdx);
+        if(dataValues[2]<xtrlCut)
+            inputHisto.Fill(dataValues[1]);
+        
+        satellitePosX.Fill(dataValues[3]);
+        satellitePosY.Fill(dataValues[4]);
+        satellitePosZ.Fill(dataValues[5]);
+
+        satelliteVelX.Fill(dataValues[6]);
+        satelliteVelY.Fill(dataValues[7]);
+        satelliteVelZ.Fill(dataValues[8]);
+
+    }
+
     //Cleanup memory ...
     dTree->Delete();
+
+    //Write telemetry histos ...
+
+    satellitePosX.Write();
+    satellitePosY.Write();
+    satellitePosZ.Write();
+
+    satelliteVelX.Write();
+    satelliteVelY.Write();
+    satelliteVelZ.Write();
 
 }
