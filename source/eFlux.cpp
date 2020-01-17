4b5bd31be180c0e9d9cf4fd4f7423251e9ac70b8
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-17 10:23:29 +0100

    Updated with vector binnings and eCounter
    eCounter has been implemented

--- source/eFlux.cpp
+++ source/eFlux.cpp
@@ -1,19 +1,31 @@
 #include "myHeader.h"
 
-void buildFlux(TFile &outFile)
+void buildFlux(TFile &outFile,const std::string inputPath)
 {
     
-    const double minValue = 3e-10;
-    const double maxValue = 5e+10;
+    double minValue = 3e-10;
+    double maxValue = 5e+10;
     const int nBins = 10;
 
     std::vector<double> logEBins = createLogBinning(minValue,maxValue,nBins);
+    
+    minValue = 10;
+    maxValue = 1e+5;
+    
+    std::vector<double> eCounts = createLinBinning(minValue,maxValue,nBins);
 
-    buildXtrlFlux(outFile,logEBins);
+    buildXtrlFlux(outFile,logEBins,eCounts,inputPath);
 
 }
 
-void buildXtrlFlux(TFile &outFile,std::vector<double> &logBins)
+void buildXtrlFlux(TFile &outFile,std::vector<double> &eBins,std::vector<double> &cBins,const std::string inputPath)
 {
+    /*
+        All-Electron flux using xtrl as classifier
+    */
+
+    TH1D eCounts("eCounts","All Electron counts - xtrl classifier",eBins.size(),&(eBins[0]));
+    evLoop(outFile,eCounts,inputPath,true);
+
 
 }
