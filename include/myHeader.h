d5827acd92a54279b9cf680a6278307b2de89695
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-29 14:56:46 +0100

    Analysis functions updated

--- include/myHeader.h
+++ include/myHeader.h
@@ -7,40 +7,62 @@
 #include <string>
 #include <vector>
 
+#include "anyoption.h"
+
 #include "TTree.h"
 #include "TFile.h"
 #include "TH1D.h"
 
 #pragma once
 
+#define nGFitLoops 1
+
 extern void eCore(
                     const std::string inputPath,
                     const std::string outputPath,
                     const bool verbose,
-                    const unsigned int lvTime
+                    const unsigned int lvTime,
+                    AnyOption &opt
                 );
 
-extern void readInputTree(const std::string inputPath,std::vector<double> &dataValues,TTree* dTree);
-extern void branchTree(TTree &myDataTree,std::vector<double> &dataValues);
-extern const char* uniqueOutFile(const std::string outputPath);
-extern std::vector<double> createLogBinning(const double minValue,const double maxValue,const int nBins);
-extern std::vector<double> createLinBinning(const double minValue,const double maxValue,const int nBins);
-extern void buildFlux(const std::string inputPath,const unsigned int lvTime);
+extern void readInputTree(const std::string inputPath,std::vector<float> &dataValues,TTree* dTree);
+extern void branchTree(TTree &myDataTree,std::vector<float> &dataValues);
+extern const char* uniqueOutFile(const std::string outputPath,AnyOption &opt);
+extern std::vector<float> createLogBinning(const double minValue,const double maxValue,const int nBins);
+extern std::vector<float> createLinBinning(const double minValue,const double maxValue,const int nBins);
+
+extern void buildFlux(
+                        const std::string inputPath,
+                        const unsigned int lvTime,
+                        TFile &outFile
+                    );
+
+extern bool chechFlags(
+                        AnyOption &opt,
+                        const std::string inputPath,
+                        const std::string outputPath,
+                        const unsigned int lvTime
+                    );
 
 extern void buildXtrlFlux(
-                            std::vector<double> &eBins,
-                            std::vector<double> &cBins,
+                            std::vector<float> &eBins,
+                            std::vector<float> &cBins,
                             const std::string inputPath,
-                            const unsigned int lvTime
+                            const unsigned int lvTime,
+                            TFile &outFile
                         );
 
 extern void evLoop(
                     TH1D &inputHisto,
                     const std::string inputPath,
+                    TFile &outFile,
                     const bool eClassifier=false,
                     const double xtrlCut=8.5
                 );
 
-extern void buildAcceptance(TH1D &acceptance);
+extern void buildAcceptance(TH1D &acceptance, TFile &outFile);
+
+extern void fitGFactor(TH1D *gFactor,TFile &outFile);
+extern double logisticFunction(double *x, double *par);
 
 #endif
