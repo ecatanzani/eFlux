aa0df98178761b3e3d18d93ea007f2711637012a
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-29 14:59:30 +0100

    Analysis functions updated with release parameters

--- source/Core.cpp
+++ source/Core.cpp
@@ -4,19 +4,20 @@ void eCore(
             const std::string inputPath,
             const std::string outputPath,
             const bool verbose,
-            const unsigned int lvTime
-            )
+            const unsigned int lvTime,
+            AnyOption &opt
+        )
 {
     //Create output TFile
-    TFile outFile(uniqueOutFile(outputPath),"NEW","Analysis Output File");
+    TFile outFile(uniqueOutFile(outputPath,opt),"NEW","Analysis Output File");
     if(!outFile.IsOpen())
     {
-        std::cerr << "\n\nError writing output TFile: " << uniqueOutFile(outputPath) << std::endl;
+        std::cerr << "\n\nError writing output TFile: " << uniqueOutFile(outputPath,opt) << std::endl;
         exit(123);
     }
     
     //Building eFLux
-    buildFlux(inputPath,lvTime);
+    buildFlux(inputPath,lvTime,outFile);
 
     //Close output file ...
     outFile.Close();
