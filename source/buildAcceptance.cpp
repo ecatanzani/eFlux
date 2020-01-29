3bac937f53a7dfeb231057bdf9416dc2426993d9
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-29 14:59:02 +0100

    Updated with output TFile and GFactor fit TGraphs
    Updated the analysis function: the output TFile has been passed to the function in order to store the geometrical factor fitting TGraphs and final fitted acceptance

--- source/buildAcceptance.cpp
+++ source/buildAcceptance.cpp
@@ -1,6 +1,6 @@
 #include "myHeader.h"
 
-void buildAcceptance(TH1D &acceptance)
+void buildAcceptance(TH1D &acceptance, TFile &outFile)
 {
     TH1D* gFactor = nullptr;
     TH1D* selEfficiency_beforeSelection = nullptr;
@@ -15,28 +15,38 @@ void buildAcceptance(TH1D &acceptance)
         std::cerr << "\n\nError opening geometric factor TFile: " << geoFilePath << std::endl;
         exit(123);
     }
-    geoFile.GetObject("Acc",gFactor);
+    
+    // Getting MC Geometrical Factor from file
+    //geoFile.GetObject("Acc",gFactor);
+    gFactor = (TH1D*) geoFile.Get("Acc");
     gFactor->SetDirectory(0);
     geoFile.Close();
-
+    fitGFactor(gFactor,outFile);
+    
     TFile selFile(selFilePath,"READ");
     if(!selFile.IsOpen())
     {
         std::cerr << "\n\nError opening efficiency selection TFile: " << selFilePath << std::endl;
         exit(123);
     }
-    selFile.GetObject("BgoTotalEcorr_fidBgo_log_L",selEfficiency_beforeSelection);
-    selFile.GetObject("BgoTotalEcorr_sel_allCases_xtrLoose_log_L",selEfficiency_alfterSelection);
+    //selFile.GetObject("BgoTotalEcorr_fidBgo_log_L",selEfficiency_beforeSelection);
+    //selFile.GetObject("BgoTotalEcorr_sel_allCases_xtrLoose_log_L",selEfficiency_alfterSelection);
+    selEfficiency_beforeSelection = (TH1D*) selFile.Get("BgoTotalEcorr_fidBgo_log_L");
+    selEfficiency_alfterSelection = (TH1D*) selFile.Get("BgoTotalEcorr_sel_allCases_xtrLoose_log_L");
     selEfficiency_beforeSelection->SetDirectory(0);
     selEfficiency_alfterSelection->SetDirectory(0);
     selFile.Close();
+    
+    /*
+        Sumw2() just applied to each single histo
 
     gFactor->Sumw2();
     selEfficiency_beforeSelection->Sumw2();
     selEfficiency_alfterSelection->Sumw2();
-
+    */
+    
     selEfficiency_alfterSelection->Divide(selEfficiency_beforeSelection);
-    gFactor->Multiply(selEfficiency_alfterSelection);
+    //gFactor->Multiply(selEfficiency_alfterSelection);
 
     new (&acceptance) (TH1D) (*(TH1D*)gFactor->Clone("Acceptance"));
     
