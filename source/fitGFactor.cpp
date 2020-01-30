2545c11d35b91a69a8c41d9246111c9a71096304
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-01-30 17:23:44 +0100

    The fit logis has been updated.
    The current model is a logistic function.

--- source/fitGFactor.cpp
+++ source/fitGFactor.cpp
@@ -4,19 +4,22 @@
 #include "TMath.h"
 #include "TGraph.h"
 #include "TDirectory.h"
+#include "TString.h"
 
 double logisticFunction(double *x, double *par)
 {
-    float xx =x[0];
-    double logistic = par[0]/( 1 + TMath::Exp(-par[1] * (xx - par[2])) );
+    double xx =x[0];
+    double logistic = par[0] + ( par[1] ) / ( 1 + TMath::Exp( - par[2] * ( par[3]*TMath::Power(xx,2) + par[4]*xx +par[5] ) ) );
     return logistic;
 }
 
-void fitGFactor(TH1D *gFactor, TFile &outFile)
+void fitGFactor(TH1D *gFactor, TFile &outFile, const bool verbose)
 {
-    int nPars = 3;
-    TF1 gFitter("gFitter",logisticFunction,0,1e+4,nPars);
+    int nPars = 6;
     
+    //TF1 gFitter("gFitter",logisticFunction,0,1e+4,nPars);
+    TF1 gFitter("gFitter","[0] + ( [1] ) / ( 1 + TMath::Exp( - [2] * ( [3]*TMath::Power(x,2) + [4]*x +[5] ) ) ) ",0,1e+4);
+
     // vectors containing the parameters values
     std::vector<double> iters(nGFitLoops,.0); 
     std::vector < std::vector <double> > fitPars(nPars, std::vector <double> (nGFitLoops));
@@ -26,44 +29,30 @@ void fitGFactor(TH1D *gFactor, TFile &outFile)
         // Add the iteration number to iters vector
         iters[fIdx] = fIdx;
         
-        for(int pIdx=0; pIdx<nPars; ++pIdx)
-        {
-            // Set initial values for the fit.
-            // In case of first iteration manually set initial values, otherwise get the fitted values obtained at the previous iteration
-            if(fIdx==0 && pIdx==0)
-            {
-                gFitter.SetParameter(0,0.3);
-                gFitter.SetParameter(1,1);
-                gFitter.SetParameter(2,4e+3);
-            }
-            else
-                for(int pIdx=0; pIdx<nPars; ++pIdx)
-                    gFitter.SetParameter(pIdx,gFitter.GetParameter(pIdx));
-            
-            // Fixing 1 parameter on 3 on the fit model...            
-            for(int fIdx=0; fIdx<3; ++fIdx)
-                if(fIdx!=pIdx)
-                    gFitter.FixParameter(fIdx,gFitter.GetParameter(fIdx));
-            
-            // Fit the model to the MC
-            gFactor->Fit("gFitter","VIWM0");
+        gFactor->Fit("gFitter","I");
 
-            // Release fixed parameter
-            for(int fIdx=0; fIdx<3; ++fIdx)
-                if(fIdx!=pIdx)
-                    gFitter.ReleaseParameter(fIdx);
-            
-        }
-
-        // Add the fit parameters value to fitPars vector
         for(int pIdx=0; pIdx<nPars; ++pIdx)
             fitPars[pIdx][fIdx] = gFitter.GetParameter(pIdx);
         
+        if(verbose)
+        {
+            std::cout << "\n\nIteration " << fIdx+1;
+            for(int pIdx=0; pIdx<nPars; ++pIdx)
+                std::cout << "\nPar " << pIdx <<"\t-> " << gFitter.GetParameter(pIdx);
+        }
     }
     
-    TGraph grPar0(nGFitLoops, & (iters[0]), & (fitPars[0][0]));
-    TGraph grPar1(nGFitLoops, & (iters[0]), & (fitPars[1][0]));
-    TGraph grPar2(nGFitLoops, & (iters[0]), & (fitPars[2][0]));
+    TGraph* grPar[nPars];
+    for(int pIdx=0; pIdx<nPars; ++pIdx)
+    {
+        TString grName = "grPar_";
+        TString grTitle = "grPar ";
+        grName += pIdx;
+        grTitle += pIdx;
+        grPar[pIdx] = new TGraph(nGFitLoops, & (iters[0]), & (fitPars[pIdx][0]));
+        grPar[pIdx]->SetName(grName);
+        grPar[pIdx]->SetTitle(grTitle);
+    }
     
     // Creating a TDirectory for the outputs of the fit procedure on the Geometrical Factor
     TDirectory *geoFactorDir = outFile.mkdir("geoFactor");
@@ -73,10 +62,13 @@ void fitGFactor(TH1D *gFactor, TFile &outFile)
     gFactor->Write();
 
     //Writing TGraphs
-    grPar0.Write();
-    grPar1.Write();
-    grPar2.Write();
+    for(int pIdx=0; pIdx<nPars; ++pIdx)
+        grPar[pIdx]->Write();
 
     // Returning to main dir on the output TFile
     outFile.cd();
+
+    // Cleaning memory
+    for(int pIdx=0; pIdx<nPars; ++pIdx)
+        grPar[pIdx]->Delete();
 }
