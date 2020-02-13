4d30d0f271816533c1bad0381d11848f8fa942af
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-02-13 15:06:42 +0100

    Removed docker building in external repo and updated related workflow

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-_CLONE="git clone git@github.com:ecatanzani/eFlux.git"
-_BUILD="cd eFlux && make rebuild"
-
-${_CLONE}
-${_BUILD}
--- setDAMPE.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source /root/setup-externals.sh
-unset DMPSWSYS
-cd /dampe/releases/DmpSoftware-6-0-10/
-source bin/thisdmpsw.sh
-
