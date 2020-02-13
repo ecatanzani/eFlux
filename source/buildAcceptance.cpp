bad02636a739734140d2ea1c79d73158869dd1a2
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-02-13 13:15:11 +0100

    Build acceptance from MC/Data

--- source/buildAcceptance.cpp
+++ source/buildAcceptance.cpp
@@ -1,6 +1,11 @@
 #include "myHeader.h"
 
+void aggregateReco(const std::string accInputPath,const bool verbose)
+{
+    //still building
+}
+
 void buildAcceptance(const std::string accInputPath,const bool verbose)
 {
-    
+    aggregateReco(accInputPath,verbose);
 }
