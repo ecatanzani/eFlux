97234f8d3cf0ee6928e69ccc03ee581ee0fc9083
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-02-13 16:38:47 +0100

    Udpated local DAMPE action

--- .github/actions/DAMPE_builder/entrypoint.sh
+++ .github/actions/DAMPE_builder/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-_CLONE="git clone /root/git@github.com:ecatanzani/eFlux.git"
-_BUILD="cd /root/eFlux && make rebuild"
+# Set DAMPE env
+source /root/setup-externals.sh
+unset DMPSWSYS
+cd /dampe/releases/DmpSoftware-6-0-10/
+source bin/thisdmpsw.sh
 
+# Clone repo
+_CLONE="git clone --recurse-submodules -b flux_computation https://github.com/ecatanzani/eFlux.git"
 ${_CLONE}
-${_BUILD}
+
+# Building
+cd eFlux
+make
