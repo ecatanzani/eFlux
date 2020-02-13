0ab1ecfe103609659d89c0b2821542aec6316341
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-02-13 15:49:51 +0100

    Update entrypoint.sh

--- .github/actions/DAMPE_builder/entrypoint.sh
+++ .github/actions/DAMPE_builder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-_CLONE="git clone git@github.com:ecatanzani/eFlux.git"
-_BUILD="cd eFlux && make rebuild"
+_CLONE="git clone /root/git@github.com:ecatanzani/eFlux.git"
+_BUILD="cd /root/eFlux && make rebuild"
 
 ${_CLONE}
 ${_BUILD}
