99df265343e72f6ef1be9f06b2bbe703632118f9
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-02-25 13:46:48 +0100

    acceptance config struct added
    Added acceptance_conf struct

--- include/acceptance.h
+++ include/acceptance.h
@@ -1,11 +1,34 @@
 #ifndef ACCEPTANCE_H
 #define ACCEPTANCE_H
 
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <memory>
+#include <fstream>
+#include <sstream>
+
+#include "DmpChain.h"
+#include "DmpEvtBgoHits.h"
+#include "DmpEvtBgoRec.h"
+#include "TSystem.h"
+#include "TVector3.h"
+
+#include <unistd.h>
+#define GetCurrentDir getcwd
+struct acceptance_conf {
+  double event_energy;
+  double energy_lRatio;
+  int shower_axis_delta;
+};
+
 extern void buildAcceptance(
                                 const std::string accInputPath,
                                 const bool verbose,
                                 const std::vector<float> &logEBins
                             );
+
+extern void load_acceptance_struct(acceptance_conf &acceptance_cuts);
 
 extern std::shared_ptr < DmpChain > aggregateEventsDmpChain(
                                                                 const std::string accInputPath,
