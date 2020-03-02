95bf1ac1fdb78eeb3ba2ccb022d63d5ec221fa0a
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-02 21:54:44 +0100

    Function to match the energy of a particle the relative energy bin
    Various methods have been used to implement this function, from the linear search to the more efficient binary search into the std::vector of the energy binning.

---
+++ include/energyMatch.h
@@ -0,0 +1,22 @@
+#ifndef ENERGYMATCH_H
+#define ENERGYMATCH_H
+
+#include <iostream>
+#include <vector>
+
+extern void allocateParticleEnergy(
+    std::vector<unsigned int> &counts,
+    const std::vector<float> &logEBins,
+    const double totalE);
+
+extern int linearSearch(
+    const std::vector<float> &logEBins, 
+    const double energy);
+
+extern int binarySearch(
+    const std::vector<float> &logEBins, 
+    int l, 
+    int r, 
+    const double energy);
+
+#endif
