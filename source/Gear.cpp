e6948cfbf061b9ff3719eaf5c9e62aad5287f14c
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-02 22:44:46 +0100

    Stuff functions reorganized

--- source/Gear.cpp
+++ source/Gear.cpp
@@ -1,47 +1,11 @@
 #include "myHeader.h"
 
-std::vector<float> createLogBinning(const double minValue, const double maxValue, const int nBins)
+std::string GetCurrentWorkingDir(void)
 {
-    std::vector<float> binning;
-
-    const double minLog = std::log(minValue);
-    const double maxLog = std::log(maxValue);
-
-    binning.resize(nBins + 1);
-
-    const double logIncrement = (maxLog - minLog) / nBins;
-
-    double value = minValue;
-    double logValue = minLog;
-
-    for (int bIdx = 0; bIdx < nBins; ++bIdx)
-    {
-        binning[bIdx] = value;
-        logValue += logIncrement;
-        value = std::exp(logValue);
-    }
-    binning[nBins] = value;
-
-    return binning;
-}
-
-std::vector<float> createLinBinning(const double minValue, const double maxValue, const int nBins)
-{
-    std::vector<float> binning;
-
-    binning.resize(nBins + 1);
-
-    const double linIncrement = (maxValue - minValue) / nBins;
-    double value = minValue;
-
-    for (int bIdx = 0; bIdx < nBins; ++bIdx)
-    {
-        binning[bIdx] = value;
-        value += linIncrement;
-    }
-    binning[nBins] = value;
-
-    return binning;
+    char buff[FILENAME_MAX];
+    GetCurrentDir(buff, FILENAME_MAX);
+    std::string current_working_dir(buff);
+    return current_working_dir;
 }
 
 bool chechFlags(
