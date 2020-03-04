9fb248507ecbc30ac1013c1f9f3f27ab75b74cba
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-04 17:28:29 +0100

    Options updated

--- source/main.cpp
+++ source/main.cpp
@@ -1,22 +1,25 @@
 #include "myHeader.h"
+#include "acceptance.h"
 
 #include <sstream>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 
     AnyOption opt;
 
     opt.addUsage("Usage: ");
     opt.addUsage("");
-    opt.addUsage(" -h  --help                                           Prints this help ");
-    opt.addUsage(" -i  --input          <path_to_input_TTree>       (*) Input data TTree ");
-    opt.addUsage(" -o  --output         <path_to_output_TFile>          Output ROOT TFile");
-    opt.addUsage(" -d  --outputDir      <path_to_output_TFile_dir>      Output ROOT TFile directory");
-    opt.addUsage(" -t  --lvtime         <live-time-value>           (*) DAMPE live-time  ");
-    opt.addUsage(" -a  --acceptance     <path_to_MC/Data_list_dir>      Acceptance calculation");
-    opt.addUsage(" -v  --verbose                                        Verbose output   ");
-    opt.addUsage(" -p  --pedantic                                       Pedantic output   ");
+    opt.addUsage(" -h  --help                                                   Prints this help");
+    opt.addUsage(" -i  --input          <path_to_input_DATA_TTree>          (*) Input data TTree - flux calculation only");
+    opt.addUsage(" -o  --output         <path_to_output_TFile>                  Output ROOT TFile");
+    opt.addUsage(" -d  --outputDir      <path_to_output_TFile_dir>              Output ROOT TFile directory");
+    opt.addUsage(" -t  --lvtime         <live_time-value>                   (*) DAMPE live-time ");
+    opt.addUsage(" -a  --acceptance     <path_to_MC_list_dir>               (*) Acceptance calculation");
+    opt.addUsage(" -f  --flux           <path_to_DATA_list_dir>             (*) Flux calculation");
+    opt.addUsage(" -g  --geometry       <path_to_acceptance_ROOT_file>      (*) Acceptance file - flux calculation only");
+    opt.addUsage(" -v  --verbose                                                Verbose output");
+    opt.addUsage(" -p  --pedantic                                               Pedantic output");
     opt.addUsage("");
 
     opt.setFlag("help", 'h');
@@ -25,6 +28,8 @@ int main(int argc, char *argv[])
     opt.setOption("outputDir", 'd');
     opt.setOption("lvtime", 't');
     opt.setOption("acceptance", 'a');
+    opt.setOption("flux", 'f');
+    opt.setOption("geometry", 'g');
     opt.setFlag("verbose", 'v');
     opt.setFlag("pedantic", 'p');
 
@@ -38,10 +43,13 @@ int main(int argc, char *argv[])
     std::string inputPath;
     std::string outputPath;
     std::string accInputPath;
+    std::string fluxInputPath;
     stringstream str_lvTime;
+
     bool verbose = false;
     bool pedantic = false;
     bool myAcceptance = false;
+    bool myFlux = false;
     unsigned int lvTime = 0;
 
     if (!opt.hasOptions())
@@ -65,37 +73,35 @@ int main(int argc, char *argv[])
         myAcceptance = true;
         accInputPath = opt.getValue('a');
     }
+    if (opt.getValue("flux") || opt.getValue('f'))
+    {
+        myFlux = true;
+        fluxInputPath = opt.getValue('f');
+    }
+    if (opt.getValue("geometry") || opt.getValue('g'))
+        accInputPath = opt.getValue('g');
     if (opt.getFlag("verbose") || opt.getFlag('v'))
         verbose = opt.getFlag('v');
     if (opt.getFlag("pedantic") || opt.getFlag('p'))
         pedantic = opt.getFlag('p');
 
-#ifdef DEBUG
+    if (myAcceptance)
+        computeAcceptance(
+            accInputPath,
+            verbose,
+            pedantic,
+            outputPath,
+            opt);
 
-    if (argc >= 5)
-    {
-        if (chechFlags(opt, inputPath, outputPath, lvTime))
-        {
-            eCore(inputPath, outputPath, verbose, lvTime, opt);
-        }
-    }
-    else
-    {
-        std::cerr << "\n\t !!! Hey mate, insert all required fields ...\n";
-        opt.printUsage();
-    }
-
-#else
-    eCore(
-        inputPath,
-        outputPath,
-        verbose,
-        pedantic,
-        lvTime,
-        myAcceptance,
-        accInputPath,
-        opt);
-#endif
+    if (myFlux)
+        eCore(
+            inputPath,
+            outputPath,
+            verbose,
+            pedantic,
+            lvTime,
+            accInputPath,
+            opt);
 
     return 0;
 }
