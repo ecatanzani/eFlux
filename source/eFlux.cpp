e582b2d7f846ebec04a276c32e0412893b86afd3
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-04 17:27:35 +0100

    Updated with XTRL flux function

--- source/eFlux.cpp
+++ source/eFlux.cpp
@@ -3,59 +3,13 @@
 #include "flux.h"
 #include "dataLoop.h"
 
-#include "TDirectory.h"
-
-void buildFlux(
-    const std::string inputPath,
-    const unsigned int lvTime,
-    TFile &outFile,
-    const bool verbose,
-    const bool pedantic,
-    const std::string accInputPath,
-    const bool myAcceptance)
-{
-    // Reading flux config from file
-    flux_conf fluxParams;
-    load_flux_struct(fluxParams);
-
-    auto logEBins = createLogBinning(fluxParams);
-
-    if (pedantic)
-    {
-        std::cout << "\nEnergy log binning..." << std::scientific;
-        for (auto it = logEBins.begin(); it != logEBins.end(); ++it)
-            std::cout << "\n"
-                      << *it;
-        std::cout << std::defaultfloat;
-    }
-
-    // Building acceptance
-    if (myAcceptance)
-        buildAcceptance(
-            accInputPath,
-            verbose,
-            logEBins,
-            outFile);
-
-    buildXtrlFlux(
-        logEBins,
-        inputPath,
-        lvTime,
-        outFile,
-        verbose,
-        myAcceptance);
-}
-
-/*
-    All-Electron flux using xtrl as classifier
-*/
 void buildXtrlFlux(
     const std::vector<float> &eBins,
     const std::string inputPath,
     const unsigned int lvTime,
     TFile &outFile,
     const bool verbose,
-    const bool myAcceptance)
+    const std::string accInputPath)
 {
     auto e_dataCounts =
         evLoop(
@@ -68,7 +22,7 @@ void buildXtrlFlux(
     auto acceptance_tf1 = readAcceptance(
         outFile,
         verbose,
-        myAcceptance);
+        accInputPath);
 
     /*
     TH1D *eFlux = nullptr;
@@ -95,26 +49,4 @@ void buildXtrlFlux(
     // Returning to main dir on the output TFile
     outFile.cd();
     */
-}
-
-std::vector<float> createLogBinning(const flux_conf flux_params)
-{
-    std::vector<float> binning(flux_params.n_energy_bins + 1, 0);
-
-    const double minLog = std::log(flux_params.min_energy);
-    const double maxLog = std::log(flux_params.max_energy);
-    const double logIncrement = (maxLog - minLog) / flux_params.n_energy_bins;
-    double value = flux_params.min_energy;
-    double logValue = minLog;
-
-    for (auto it = binning.begin(); it != (binning.end() - 1); ++it)
-    {
-        auto bIdx = std::distance(binning.begin(), it);
-        binning[bIdx] = value;
-        logValue += logIncrement;
-        value = std::exp(logValue);
-    }
-    binning[flux_params.n_energy_bins] = value;
-
-    return binning;
 }
