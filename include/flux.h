0b3d33545d50f553efe7329251e149257cec8169
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-04 17:21:28 +0100

    Header updated

--- include/flux.h
+++ include/flux.h
@@ -9,32 +9,12 @@
 
 #include "TFile.h"
 
-struct flux_conf
-{
-    double min_energy;
-    double max_energy;
-    unsigned int n_energy_bins;
-};
-
-extern std::vector<float> createLogBinning(const flux_conf flux_params);
-
-extern void buildFlux(
-    const std::string inputPath,
-    const unsigned int lvTime,
-    TFile &outFile,
-    const bool verbose,
-    const bool pedantic,
-    const std::string accInputPath,
-    const bool myAcceptance);
-
 extern void buildXtrlFlux(
     const std::vector<float> &eBins,
     const std::string inputPath,
     const unsigned int lvTime,
     TFile &outFile,
     const bool verbose,
-    const bool myAcceptance);
-
-extern void load_flux_struct(flux_conf &flux_params);
+    const std::string accInputPath);
 
 #endif
