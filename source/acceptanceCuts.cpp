87b8bcbea766a9991e12140ad6b0e9de9d023330
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-26 10:35:47 +0100

    PSD_charge cut has been implemented
    PSD clusterization complete

--- source/acceptanceCuts.cpp
+++ source/acceptanceCuts.cpp
@@ -253,3 +253,151 @@ bool xtrl_cut(
 
     return passed_xtrl_cut;
 }
+
+bool psd_charge_cut(
+    const std::shared_ptr<DmpEvtPsdHits> psdhits,
+    const acceptance_conf &acceptance_cuts)
+{
+    bool passed_psd_charge_cut = false;
+
+    std::vector<short> vshort;
+    std::vector<double> vdouble;
+    std::vector<std::vector<short>> layerBarIndexPsd;     // Arrange PSD hit index
+    std::vector<std::vector<short>> layerBarNumberPsd;    // Arrange PSD unique bar number
+    std::vector<std::vector<double>> layerBarEnergyPsd;   // Arrange PSD hit energy
+    std::vector<std::vector<short>> layerBarUsedPsd;      // mark PSD hits index used for clustering
+    std::vector<std::vector<double>> psdClusterMaxECoord; // Arrange X and Y coordinates regarding the max energy release on PSD layer
+
+    // Find max index and energy release for PSD hits
+    std::vector<unsigned int> iMaxBarPsd(2, -1);
+    std::vector<double> eMaxBarPsd(2, 0);
+    bool first_hit = true;
+
+    // Create vector matrix
+    for (int nLayer = 0; nLayer < 2; ++nLayer)
+    {
+        layerBarIndexPsd.push_back(vshort);
+        layerBarNumberPsd.push_back(vshort);
+        layerBarEnergyPsd.push_back(vdouble);
+        layerBarUsedPsd.push_back(vshort);
+        psdClusterMaxECoord.push_back(vdouble);
+    }
+
+    // Loop on PSD hits
+    int nPSD_tot_entries = psdhits->GetHittedBarNumber();
+    for (int ihit = 0; ihit < nPSD_tot_entries; ++ihit)
+    {
+        double hitE = (psdhits->fEnergy)[ihit];
+        if (hitE > 0.5)
+        {
+            short layerID = psdhits->GetLayerID(ihit);
+            int iBar = ((psdhits->fGlobalBarID)[ihit] >> 6);
+            layerBarUsedPsd[layerID].push_back(0);
+            layerBarIndexPsd[layerID].push_back(ihit);
+            layerBarNumberPsd[layerID].push_back(iBar);
+            layerBarEnergyPsd[layerID].push_back(hitE);
+            if (first_hit)
+            {
+                eMaxBarPsd[layerID] = hitE;
+                iMaxBarPsd[layerID] = layerBarIndexPsd[layerID].size() - 1;
+                first_hit = false;
+            }
+            else
+            {
+                if (hitE > eMaxBarPsd[layerID])
+                {
+                    eMaxBarPsd[layerID] = hitE;
+                    iMaxBarPsd[layerID] = layerBarIndexPsd[layerID].size() - 1;
+                }
+            }
+        }
+    }
+
+    /*
+        cluster finding : starting from maxE, seed + the highest E neigboring bar
+        coordinate: weighted average of two highest neigboring bars
+    */
+
+    for (int nLayer = 0; nLayer < 2; ++nLayer)
+    {
+        while (eMaxBarPsd[nLayer] > acceptance_cuts.PSD_bar_min_energy_release)
+        {
+            int psdhits_maxIdx = layerBarIndexPsd[nLayer][iMaxBarPsd[nLayer]];
+            int psdbar_maxIdx = layerBarNumberPsd[nLayer][iMaxBarPsd[nLayer]];
+            double cluster_energy = eMaxBarPsd[nLayer];
+            double actual_coordinate = nLayer % 2 ? psdhits->GetHitX(psdhits_maxIdx) : psdhits->GetHitY(psdhits_maxIdx);
+            psdClusterMaxECoord[nLayer].push_back(actual_coordinate);
+            double actualZ = psdhits->GetHitZ(psdhits_maxIdx);
+            layerBarUsedPsd[nLayer][iMaxBarPsd[nLayer]] = 1;
+
+            int cluster_closest_barIdx = -1;
+            double energy_leftMaxBar = 0;
+            double energy_rightMaxBar = 0;
+            int cluster_fIdx = psdhits_maxIdx; // First index of the cluster
+            int cluster_size = 1;              // Size of the cluster
+
+            // Complete the cluster building procedure with the closest bars to the one with the maximum energy release
+            if (iMaxBarPsd[nLayer] - 1 > 0) // Check the previous bar
+                if ((layerBarUsedPsd[nLayer][iMaxBarPsd[nLayer] - 1] == 0) && (psdbar_maxIdx == layerBarNumberPsd[nLayer][iMaxBarPsd[nLayer] - 1] + 1))
+                    energy_leftMaxBar = layerBarEnergyPsd[nLayer][iMaxBarPsd[nLayer] - 1];
+            if ((iMaxBarPsd[nLayer] + 1) < layerBarEnergyPsd[nLayer].size()) // Check the next bar
+                if ((layerBarUsedPsd[nLayer][iMaxBarPsd[nLayer] + 1] == 0) && (psdbar_maxIdx == layerBarNumberPsd[nLayer][iMaxBarPsd[nLayer] + 1] - 1))
+                    energy_rightMaxBar = layerBarEnergyPsd[nLayer][iMaxBarPsd[nLayer] + 1];
+
+            /* 
+                FINAL CLUSTER BUILING:
+                Now we have the central bar with the highest energy release, the left and the right one. 
+                One cluster is composed by 2 bars. We have to choose, between the 2 lateral bars, that one with the highest energy release
+            */
+            if (energy_leftMaxBar > energy_rightMaxBar)
+            {
+                cluster_closest_barIdx = psdhits_maxIdx - 1;
+                cluster_energy += energy_leftMaxBar;
+                ++cluster_size;
+                cluster_fIdx = psdhits_maxIdx - 1;
+                layerBarUsedPsd[nLayer][iMaxBarPsd[nLayer] - 1] = 1;
+            }
+            //else if (energy_leftMaxBar < energy_rightMaxBar)
+            else    
+            {
+                cluster_closest_barIdx = psdhits_maxIdx + 1;
+                cluster_energy += energy_rightMaxBar;
+                ++cluster_size;
+                layerBarUsedPsd[nLayer][iMaxBarPsd[nLayer] + 1] = 1;
+            }
+
+            if (cluster_closest_barIdx > -1)
+            {
+                double cluster_closest_bar_coordinate = iMaxBarPsd[nLayer] % 2 ? psdhits->GetHitX(cluster_closest_barIdx) : psdhits->GetHitY(cluster_closest_barIdx);
+                double cluster_closest_energy = (psdhits->fEnergy)[cluster_closest_barIdx];
+                double cluster_closestZ = psdhits->GetHitZ(cluster_closest_barIdx);
+                actual_coordinate = (actual_coordinate * eMaxBarPsd[nLayer] + cluster_closest_bar_coordinate * cluster_closest_energy) / (eMaxBarPsd[nLayer] + cluster_closest_energy);
+                actualZ = (actualZ * eMaxBarPsd[nLayer] + cluster_closestZ * cluster_closest_energy) / (eMaxBarPsd[nLayer] + cluster_closest_energy);
+            }
+
+            /*
+            psdClusterIndBeg[lay].push_back(indFirst);
+            psdClusterLength[lay].push_back(cluSize);
+            psdClusterIndMaxE[lay].push_back(imax);
+            psdClusterMaxE[lay].push_back(eMaxBarPsd[lay]);
+            psdClusterE[lay].push_back(cluE);
+            psdClusterCoord[lay].push_back(thisCoord);
+            psdClusterZ[lay].push_back(thisZ);
+            */
+
+            // Search for the next maximum energy release bar
+            eMaxBarPsd[nLayer] = 0; // Reset maximum energy release
+            for (unsigned int barIdx = 0; barIdx < layerBarEnergyPsd[nLayer].size(); ++barIdx)
+            {   
+                // Check for the next unused PSD bar with a non negligible energy release
+                if (layerBarUsedPsd[nLayer][barIdx] == 0 && layerBarEnergyPsd[nLayer][barIdx] > eMaxBarPsd[nLayer])
+                {
+                    iMaxBarPsd[nLayer] = barIdx;
+                    eMaxBarPsd[nLayer] = layerBarEnergyPsd[nLayer][barIdx];
+                }
+            }
+        }
+    }
+
+    return passed_psd_charge_cut;
+}
