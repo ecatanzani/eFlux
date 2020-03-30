6eaa7f94d6cbf2977f8db0004f1805effeb38a1c
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-30 12:46:46 +0200

    Created new 'remote builder' script
    This script permits the remote building of the project, through a Docker container, on a remote server. Thsi building system is integrated using an on-push github action

---
+++ remoteBuilder/buildProject.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /home/eFlux
+make > make.log 2>make.err
---
+++ remoteBuilder/setDAMPE.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source /root/setup-externals.sh
+unset DMPSWSYS
+cd /dampe/releases/DmpSoftware-6-0-10/
+source bin/thisdmpsw.sh
+
