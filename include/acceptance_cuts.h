9688b9d5dc5120f7bf4486c8d94cd4b54f24efcd
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-03-31 17:20:57 +0200

    Created 'best_track' class
    Updated with best track parameters

--- include/acceptance_cuts.h
+++ include/acceptance_cuts.h
@@ -12,6 +12,22 @@
 #include "DmpEvtBgoRec.h"
 #include "DmpEvtSimuPrimaries.h"
 
+struct best_track
+{
+    unsigned int n_points = 0;
+    unsigned int n_holes_X = 0;
+    unsigned int n_holes_Y = 0;
+    std::vector<double> track_slope = {-999,-999};
+    std::vector<double> track_intercept = {-999,-999};
+    TVector3 track_direction;
+    unsigned int track_idx = 0;
+    double extr_BGO_topX = -999;
+    double extr_BGO_topY = -999;
+    double STK_BGO_topX_distance = -999;
+    double STK_BGO_topY_distance = -999;
+    double angular_distance_STK_BGO = -999;
+};
+
 // DAMPE struct
 #define DAMPE_bgo_nLayers 14
 
