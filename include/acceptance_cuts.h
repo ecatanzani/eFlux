71eb6f0d87d863430f914beb305060af446f1cb9
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-04-03 18:47:29 +0200

    Updated including TrackHelper + 'best_track' structure

--- include/acceptance_cuts.h
+++ include/acceptance_cuts.h
@@ -11,16 +11,15 @@
 #include "DmpEvtBgoHits.h"
 #include "DmpEvtBgoRec.h"
 #include "DmpEvtSimuPrimaries.h"
+#include "DmpStkTrackHelper.h"
 
 struct best_track
 {
     unsigned int n_points = 0;
-    unsigned int n_holes_X = 0;
-    unsigned int n_holes_Y = 0;
+    std::vector<unsigned int> n_holes = {0, 0};
     std::vector<double> track_slope = {-999,-999};
     std::vector<double> track_intercept = {-999,-999};
     TVector3 track_direction;
-    unsigned int track_idx = 0;
     double extr_BGO_topX = -999;
     double extr_BGO_topY = -999;
     double STK_BGO_topX_distance = -999;
@@ -30,10 +29,11 @@ struct best_track
 
 // DAMPE struct
 #define DAMPE_bgo_nLayers 14
+#define nSTKladders 192
 
 extern bool maxElater_cut(
     const std::shared_ptr<DmpEvtBgoRec> bgorec,
-    const acceptance_conf &acceptance_cuts,
+    const acceptance_conf acceptance_cuts,
     const double bgoTotalE);
 
 extern bool maxBarLayer_cut(
@@ -42,7 +42,7 @@ extern bool maxBarLayer_cut(
 
 extern bool BGOTrackContainment_cut(
     const std::shared_ptr<DmpEvtBgoRec> bgorec,
-    const acceptance_conf &acceptance_cuts,
+    const acceptance_conf acceptance_cuts,
     bool &passEvent);
 
 extern bool nBarLayer13_cut(
@@ -54,21 +54,23 @@ extern bool maxRms_cut(
     const std::vector<std::vector<short>> &layerBarNumber, 
     const std::vector<double> &rmsLayer, 
     const double bgoTotalE,
-    const acceptance_conf &acceptance_cuts);
+    const acceptance_conf acceptance_cuts);
 
 extern bool track_selection_cut(
     const std::shared_ptr<DmpEvtBgoRec> bgorec,
+    const std::shared_ptr<DmpEvtBgoHits> bgohits,
     TClonesArray* stkclusters,
     TClonesArray* stktracks, 
-    const acceptance_conf &acceptance_cuts);
+    const acceptance_conf acceptance_cuts,
+    best_track &event_best_track);
     
 extern bool xtrl_cut(
     const double sumRms,
     const std::vector<double> fracLayer,
-    const acceptance_conf &acceptance_cuts);
+    const acceptance_conf acceptance_cuts);
     
 extern bool psd_charge_cut(
     const std::shared_ptr<DmpEvtPsdHits> psdhits, 
-    const acceptance_conf &acceptance_cuts);
+    const acceptance_conf acceptance_cuts);
     
 #endif
