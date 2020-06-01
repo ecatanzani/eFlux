83cd0ddcc4e4ac407cdf257273a1fee0f4cdbda2
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-06-01 18:12:43 +0200

    Crawler has been updated providing data list

--- Assets/Crawler/explorer.py
+++ Assets/Crawler/explorer.py
@@ -7,8 +7,7 @@ import subprocess
 start = datetime.now()
 
 def parseConfigFile(opts):
-    dConfig = {'farmAddress': "", 'simu_XRDFS_path': "", 'geometry': "",
-               'simu_eMin': 0, 'simu_eMax': 0, 'particle': "", 'jSet': ""}
+    dConfig = {'farmAddress': "", 'simu_XRDFS_path': "", 'data_XRDFS_path': "", 'geometry': "", 'simu_eMin': 0, 'simu_eMax': 0, 'data_eMin': 0, 'data_eMax': 0, 'particle': "", 'jSet': ""}
     
     config_params = []
     custom_farm = False
@@ -35,12 +34,18 @@ def parseConfigFile(opts):
                 dConfig['farmAddress'] = config_params[idx+1]
         if word == "simu_XRDFS_path":
             dConfig['simu_XRDFS_path'] = config_params[idx+1]
+        if word == "data_XRDFS_path":
+            dConfig['data_XRDFS_path'] = config_params[idx+1]
         if word == "geometry":
             dConfig['geometry'] = config_params[idx+1]
         if word == "simu_eMin":
             dConfig['simu_eMin'] = float(config_params[idx+1])
         if word == "simu_eMax":
             dConfig['simu_eMax'] = float(config_params[idx+1])
+        if word == "data_eMin":
+            dConfig['data_eMin'] = float(config_params[idx+1])
+        if word == "data_eMax":
+            dConfig['data_eMax'] = float(config_params[idx+1])
         if word == "particle":
             dConfig['particle'] = config_params[idx+1]
         if word == "jSet":
@@ -49,7 +54,7 @@ def parseConfigFile(opts):
     
     return dConfig
 
-def createOutFile(opts, simu_eMin, simu_eMax, particle):
+def createOutSimuFile(opts, simu_eMin, simu_eMax, particle):
     if not opts.input:
         finalPath = "simuFileList_"
         finalPath += particle
@@ -57,6 +62,17 @@ def createOutFile(opts, simu_eMin, simu_eMax, particle):
         finalPath += str(simu_eMin)
         finalPath += "_"
         finalPath += str(simu_eMax)
+        finalPath += ".txt"
+        return finalPath
+    else:
+        return opts.output
+
+def createOutSimuFile(opts, data_eMin, data_eMax):
+    if not opts.input:
+        finalPath = "dataFileList_"
+        finalPath += str(data_eMin)
+        finalPath += "_"
+        finalPath += str(data_eMax)
         finalPath += ".txt"
         return finalPath
     else:
@@ -74,6 +90,8 @@ def main(args=None):
                         dest='farm', help='MC ROOT input farm')
     parser.add_argument("-o", "--output", type=str,
                         dest='output', help='output file list')
+    parser.add_argument("-d", "--data", dest='data', default=False,
+                        action='store_true', help='get DATA file list')
     parser.add_argument("-v", "--verbose", dest='verbose', default=False,
                         action='store_true', help='run in high verbosity mode')
 
@@ -82,32 +100,61 @@ def main(args=None):
     # Get dictionary from config file parsing
     pars = parseConfigFile(opts)
 
-    # Parse data json
-    with open(pars['jSet'], 'rb') as set_:
-        dataSets = json.load(set_)
+    if opts.data:
+        dList=[]
+        if pars['geometry'] == "6r0p0":
+            geoPars = "6.0.0"
+        if pars['geometry'] == "5r4p0":
+            geoPars = "5.4.0"
+        dataPath = pars['data_XRDFS_path'] + geoPars
 
-        # Create out file list
-        dList = []
-        for elm in dataSets:
-            if elm["geometry"] == pars['geometry']:
-                if elm["particle"] == pars['particle']:
-                    if int(elm["eMin"]) >= pars['simu_eMin']:
-                        if int(elm["eMax"]) <= pars['simu_eMax']:
-                            tmpSetPath = pars['simu_XRDFS_path'] + "v" + \
-                                pars['geometry'] + "/" + elm["dSet"]
-                            dList.append(tmpSetPath)
-
-    with open(createOutFile(opts, pars['simu_eMin'], pars['simu_eMax'], pars['particle']), "w") as outList:
-        for set_ in dList:
-            tmpCommand = 'xrdfs {} ls {}'.format(pars['farmAddress'], set_)
-            if opts.verbose:
-                print(tmpCommand)
-            tmpOut = subprocess.run(
-                tmpCommand, shell=True, check=True, stdout=subprocess.PIPE)
-            setList = str.split(tmpOut.stdout.decode('utf-8').rstrip(), '\n')
-            for file in setList:
+        getDataDirsCommand = 'xrdfs {} ls {}'.format(pars['farmAddress'], dataPath)
+        if opts.verbose:
+            print(getDataDirsCommand)
+        dataDirsOut = subprocess.run(
+            getDataDirsCommand, shell=True, check=True, stdout=subprocess.PIPE)
+        dataDirs = str.split(dataDirsOut.stdout.decode('utf-8').rstrip(), '\n')
+        for elm in dataDirs:
+            tmpDataCommand = 'xrdfs {} ls {}'.format(pars['farmAddress'], elm)
+            tmpData = subprocess.run(
+                tmpDataCommand, shell=True, check=True, stdout=subprocess.PIPE)
+            dataInDir = str.split(tmpData.stdout.decode('utf-8').rstrip(), '\n')
+            for sData in dataInDir:
+                if sData.endswith('.root'):
+                    dataTmpCompletePath = pars['farmAddress'] + sData
+                    dList.append(dataTmpCompletePath)
+        with open(createOutDataFile(opts, pars['data_eMin'], pars['data_eMax']), "w") as outList:
+            for file in dList:
                 outPath = pars['farmAddress'] + file + "\n"
                 outList.write(outPath)
+
+    else:
+        # Parse data json
+        with open(pars['jSet'], 'rb') as set_:
+            dataSets = json.load(set_)
+
+            # Create out file list
+            dList = []
+            for elm in dataSets:
+                if elm["geometry"] == pars['geometry']:
+                    if elm["particle"] == pars['particle']:
+                        if int(elm["eMin"]) >= pars['simu_eMin']:
+                            if int(elm["eMax"]) <= pars['simu_eMax']:
+                                tmpSetPath = pars['simu_XRDFS_path'] + "v" + \
+                                    pars['geometry'] + "/" + elm["dSet"]
+                                dList.append(tmpSetPath)
+
+        with open(createOutSimuFile(opts, pars['simu_eMin'], pars['simu_eMax'], pars['particle']), "w") as outList:
+            for set_ in dList:
+                tmpCommand = 'xrdfs {} ls {}'.format(pars['farmAddress'], set_)
+                if opts.verbose:
+                    print(tmpCommand)
+                tmpOut = subprocess.run(
+                    tmpCommand, shell=True, check=True, stdout=subprocess.PIPE)
+                setList = str.split(tmpOut.stdout.decode('utf-8').rstrip(), '\n')
+                for file in setList:
+                    outPath = pars['farmAddress'] + file + "\n"
+                    outList.write(outPath)
 
 
 if __name__ == "__main__":
