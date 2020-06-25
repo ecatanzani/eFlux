a489a37f13e99bab79759bc35767e64e1bbbbbd7
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-06-25 15:00:36 +0200

    Updated with a smarter file check
    Here the checks implemented:
    - check if output HTCondor dir exists
    - check if output ROOT file exists
    - check if output ROOT file is corrupted
    - check if output ROOT file has keys

--- Assets/acceptanceAdder/moduls/scanDirs.py
+++ Assets/acceptanceAdder/moduls/scanDirs.py
@@ -1,14 +1,24 @@
 import os
+import shutil
 from ROOT import TFile
 
 
 def getListOfFiles(condor_wd):
     list_dir = []
     skipped_dirs = []
+
+    skipped_file_notFinalDir = 0
+    skipped_file_notROOTfile = 0
+    skipped_file_notReadable = 0
+    skipped_file_noKeys = 0
+
+    # Starting loop on output condor dirs
     for tmp_dir in os.listdir(condor_wd):
         if tmp_dir.startswith('job_'):
             full_dir_path = condor_wd + "/" + tmp_dir
             expected_condor_outDir = full_dir_path + "/outFiles"
+            
+            # Check if 'outFiles' dir exists
             if os.path.isdir(expected_condor_outDir):
                 _list_dir = os.listdir(expected_condor_outDir)
                 tmp_acc_full_path = ""
@@ -16,14 +26,38 @@ def getListOfFiles(condor_wd):
                     if file.endswith(".root"):
                         tmp_acc_full_path = expected_condor_outDir + "/" + file
                         break
+                
+                # Check if output ROOT file exists
                 if os.path.isfile(tmp_acc_full_path):
                     tmp_acc_file = TFile.Open(tmp_acc_full_path, "READ")
+                    
+                    # Check if output ROOT file is redable
                     if tmp_acc_file.IsOpen():
-                        list_dir.append(full_dir_path)
+                        
+                        # Check if output ROOT file has keys
+                        outKeys = tmp_acc_file.GetNkeys()
+                        
+                        if outKeys:
+                            list_dir.append(full_dir_path)
+
+                        else:
+                            # output ROOT file has been open but has not keys
+                            skipped_dirs.append(full_dir_path)
+                            skipped_file_noKeys += 1
+
                     else:
+                        # output ROOT file has not been opened correctly
                         skipped_dirs.append(full_dir_path)
+                        skipped_file_notReadable += 1
+
                 else:
+                    # output ROOT file does not exist
                     skipped_dirs.append(full_dir_path)
+                    skipped_file_notROOTfile += 1
+            
             else:
+                # 'outFiles' dir does not exists
                 skipped_dirs.append(full_dir_path)
-    return list_dir, skipped_dirs
+                skipped_file_notFinalDir += 1
+
+    return list_dir, skipped_dirs, skipped_file_notFinalDir, skipped_file_notROOTfile, skipped_file_notReadable, skipped_file_noKeys
