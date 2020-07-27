67e9c2d05d4001491cf3ff53e107ad773a6471f7
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-07-27 17:01:57 +0200

    Deep code refactory + new template charge functions + new charge structs
    Deep code refactory. PSD and STK charges hasve their structs where the values will be stored.
    The template analysis function (that works on both PSD and STK charges) read from these structs withoud calculationg charges every time

--- include/charge.h
+++ include/charge.h
@@ -6,12 +6,25 @@
 #include "TH1D.h"
 #include "TH2D.h"
 
-extern void fillChargeHistos(
-    TH1D &h_chargeX, 
-    TH1D &h_chargeY,
-    TH1D &h_charge,
-    TH2D &h_charge2D,
-    const best_track track,
-    const std::shared_ptr<TClonesArray> stkclusters);
+template <typename InputChargeStruct> extern void fillChargeHistos(
+    TH1D &h_chargeX,
+	TH1D &h_chargeY,
+	TH1D &h_charge,
+	TH2D &h_charge2D,
+    const InputChargeStruct charge_struct);
+
+extern template void fillChargeHistos(
+    TH1D &h_chargeX,
+	TH1D &h_chargeY,
+	TH1D &h_charge,
+	TH2D &h_charge2D,
+	const stk_charge extracted_stk_charge);
+
+extern template void fillChargeHistos(
+	TH1D &h_chargeX,
+	TH1D &h_chargeY,
+	TH1D &h_charge,
+	TH2D &h_charge2D,
+	const psd_charge extracted_psd_charge);
 
 #endif
--- source/charge.cpp
+++ source/charge.cpp
@@ -1,42 +1,37 @@
 #include "charge.h"
 
-void fillChargeHistos(
-    TH1D &h_chargeX, 
-    TH1D &h_chargeY,
-    TH1D &h_charge,
-    TH2D &h_charge2D,
-    const best_track track,
-    const std::shared_ptr<TClonesArray> stkclusters)
+
+template <typename InputChargeStruct> void fillChargeHistos(
+    TH1D &h_chargeX,
+	TH1D &h_chargeY,
+	TH1D &h_charge,
+	TH2D &h_charge2D,
+    const InputChargeStruct charge_struct)
 {
-    double cluster_chargeX = -999;
-    double cluster_chargeY = -999;
-    auto btrack = track.myBestTrack;
-    
-    // Charge correction 
-    auto track_correction = btrack.getDirection().CosTheta();
+	// Check charges
+	if (charge_struct.chargeX != -999 && charge_struct.chargeY != -999)
+	{
+		// Compute mean charge
+		auto mean_charge = 0.5 * (charge_struct.chargeX + charge_struct.chargeY);
 
-    // Compute charges
-    for(auto clIdx=0; clIdx<track.n_points; ++clIdx)
-    {
-        auto cluster_x = btrack.GetClusterX(clIdx, stkclusters.get());
-        auto cluster_y = btrack.GetClusterY(clIdx, stkclusters.get());
-        if (cluster_x && !cluster_x->getPlane())
-            cluster_chargeX = cluster_x->getEnergy()*track_correction;
-        if (cluster_y && !cluster_y->getPlane())
-            cluster_chargeY = cluster_y->getEnergy()*track_correction;
-    }
-
-    // Check charges
-    if (cluster_chargeX != -999 && cluster_chargeY != -999)
-    {
-        // Compute mean charge
-        auto mean_charge = 0.5*(cluster_chargeX + cluster_chargeY);
-        
-        // Fill histos
-        h_chargeX.Fill(cluster_chargeX);
-        h_chargeY.Fill(cluster_chargeY);
-        h_charge.Fill(mean_charge);
-        h_charge2D.Fill(cluster_chargeX, cluster_chargeY);
-    }
-
+		// Fill histos
+		h_chargeX.Fill(charge_struct.chargeX);
+		h_chargeY.Fill(charge_struct.chargeY);
+		h_charge.Fill(mean_charge);
+		h_charge2D.Fill(charge_struct.chargeX, charge_struct.chargeY);
+	}
 }
+
+template void fillChargeHistos(
+    TH1D &h_chargeX,
+	TH1D &h_chargeY,
+	TH1D &h_charge,
+	TH2D &h_charge2D,
+	const stk_charge extracted_stk_charge);
+
+template void fillChargeHistos(
+	TH1D &h_chargeX,
+	TH1D &h_chargeY,
+	TH1D &h_charge,
+	TH2D &h_charge2D,
+	const psd_charge extracted_psd_charge);
