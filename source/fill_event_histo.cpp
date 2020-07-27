ca8b13869c0101b013b55cb0f428ebc2b5cdcaf2
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-07-27 17:09:13 +0200

    New e/p discrimination histos + code refactory

--- source/fill_event_histo.cpp
+++ source/fill_event_histo.cpp
@@ -1,28 +1,40 @@
 #include "fill_event_histo.h"
 
 void fill_XTRL_histo(
-    const double sumRms,
-    const std::vector<double> fracLayer,
-    const cuts_conf data_cuts,
-    const double energy,
-    TH1D &h_xtrl_energy_int,
-    TH2D &h_xtrl)
+	const double sumRms,
+	const double lastFracLayer,
+	const double energy,
+	std::vector<std::shared_ptr<TH1D>> &bin_xtrl,
+	TH1D &h_xtrl_energy_int,
+	TH2D &h_xtrl)
 {
-    unsigned int last_layer_idx = 0;
-    double _GeV = 0.001;
+	double _GeV = 0.001;
 
-    // Find the last layer with an energy release
-    for (auto it = fracLayer.begin(); it != fracLayer.end(); ++it)
-        if (*it)
-        {
-            auto index = std::distance(fracLayer.begin(), it);
-            last_layer_idx = index;
-        }
+	if (lastFracLayer != -1)
+	{
+		auto xtrl = 0.125e-6 * pow(sumRms, 4) * lastFracLayer;
+		
+		// Fill XTRL histos
+		h_xtrl_energy_int.Fill(xtrl);
+		h_xtrl.Fill(energy * _GeV, xtrl);
+		bin_xtrl[(h_xtrl.GetXaxis()->FindBin(energy * _GeV)) - 1]->Fill(xtrl);
+	} 
+}
 
-    // Build XTRL
-    double xtrl = 0.1251e-8 * pow(sumRms, 4) * fracLayer[last_layer_idx];
-
-    // Fill XTRL histos
-    h_xtrl_energy_int.Fill(xtrl);
-    h_xtrl.Fill(energy * _GeV, xtrl);
+void fill_ep_histos(
+	const double sumRMS,
+	const double lastFracLayer,
+	const double frac_layer_13,
+	TH2D &e_discrimination,
+	TH2D &e_discrimination_last)
+{
+	if (lastFracLayer != -1 && frac_layer_13 != -1)
+	{
+		e_discrimination.Fill(
+			sumRMS, 
+			lastFracLayer);
+		e_discrimination_last.Fill(
+			sumRMS, 
+			frac_layer_13);
+	}
 }
