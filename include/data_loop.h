5195aec0c6fd07c5b0560fb37715f69f62e9bbd4
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-07-29 10:54:09 +0200

    New data statistics and filter structures

--- include/data_loop.h
+++ include/data_loop.h
@@ -12,9 +12,11 @@
 #include "TH1D.h"
 #include "TClonesArray.h"
 
+#include "DmpBgoContainer.h"
+#include "DmpPsdContainer.h"
+
 // DAMPESW includes
 #include "DmpChain.h"
-#include "DmpBgoContainer.h"
 #include "DmpEvtHeader.h"
 #include "DmpEvtSimuPrimaries.h"
 #include "DmpEvtBgoHits.h"
@@ -23,11 +25,35 @@
 #include "DmpStkTrack.h"
 #include "DmpEvtPsdHits.h"
 
+struct data_statistics
+{
+    unsigned int event_counter = 0;
+    unsigned int events_in_range = 0;
+    unsigned int events_out_range = 0;
+    unsigned int events_in_saa = 0;
+    unsigned int triggered_events = 0;
+    unsigned int selected_events = 0;
+};
+
 extern TH1D evLoop(
     const std::string inputPath,
     TFile &outFile,
     const bool verbose,
     const std::string wd);
+
+extern bool filter_this_data_event(
+    event_filter &filter,
+    const std::shared_ptr<DmpEvtBgoRec> &bgorec,
+    const std::shared_ptr<DmpEvtBgoHits> &bgohits,
+    const cuts_conf &flux_cuts,
+    const double bgoTotalE,
+	DmpBgoContainer &bgoVault,
+	DmpPsdContainer &psdVault,
+	psd_charge &extracted_psd_charge,
+	stk_charge &extracted_stk_charge,
+    const std::shared_ptr<TClonesArray> &stkclusters,
+    const std::shared_ptr<TClonesArray> &stktracks,
+	const data_active_cuts &active_cuts);
 
 extern void readInputTree(
     const std::string inputPath,
