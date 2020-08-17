ab30bfd81657f0672420e047c5e27d13f6510bcd
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-08-17 09:45:46 +0200

    Code reorganized considering the ntuple production facility

--- source/Core.cpp
+++ source/Core.cpp
@@ -11,24 +11,33 @@ void eCore(
     const std::string accInputPath,
     AnyOption &opt,
     const std::string wd)
-{
-    // Create output TFile
-    const char* outFilePath = static_cast<const char*>(uniqueOutFile(outputPath, opt).c_str());
-    TFile outFile(outFilePath, "NEW", "Analysis Output File");
-    if (!outFile.IsOpen())
+{   
+    if (opt.getValue("ntuple") || opt.getValue('n'))
+        produceTuples(
+            opt,
+            inputPath,
+            verbose,
+            wd);
+    else
     {
-        std::cerr << "\n\nError writing output TFile: " << outFilePath << std::endl;
-        exit(123);
-    }
-    
-    buildFlux(
-        inputPath,
-        lvTime,
-        outFile,
-        verbose,
-        accInputPath,
-        wd);
+        // Create output TFile
+        const char* outFilePath = static_cast<const char*>(uniqueOutFile(outputPath, opt).c_str());
+        TFile outFile(outFilePath, "NEW", "Analysis Output File");
+        if (!outFile.IsOpen())
+        {
+            std::cerr << "\n\nError writing output TFile: " << outFilePath << std::endl;
+            exit(123);
+        }
 
-    // Close output file ...
-    outFile.Close();
+        buildFlux(
+            inputPath,
+            lvTime,
+            outFile,
+            verbose,
+            accInputPath,
+            wd);
+
+        // Close output file ...
+        outFile.Close();
+    }
 }
