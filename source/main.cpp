ecce95f9c7ee22f1e9b535044b07645b16cecc1d
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-08-19 19:32:34 +0200

    Input fflags updated
    Updated including electrons/protons acceptance files

--- source/main.cpp
+++ source/main.cpp
@@ -10,19 +10,20 @@ int main(int argc, char **argv)
 
 	opt.addUsage("Usage: ");
 	opt.addUsage("");
-	opt.addUsage(" -h  --help                                                   Prints this help");
-	opt.addUsage(" -i  --input          <path_to_input_DATA_list>           (*) Input DATA list");
-	opt.addUsage(" -w  --workdir        <path_to_software_config_dir>       (*) Config directory");
-	opt.addUsage(" -o  --output         <path_to_output_TFile>                  Output ROOT TFile");
-	opt.addUsage(" -d  --outputDir      <path_to_output_TFile_dir>              Output ROOT TFile directory");
-	opt.addUsage(" -t  --lvtime         <live_time-value>                   (*) DAMPE live-time ");
-	opt.addUsage(" -a  --acceptance     <path_to_MC_list>                   (*) Acceptance calculation");
-	opt.addUsage(" -n  --ntuple                                             (*) nTuple production facility");
-	opt.addUsage(" -c  --collect        <path_to_complete_histo>            (*) Generate TGraph from final histo");
-	opt.addUsage(" -r  --resume         <mc/data>                           (*) MC/DATA facility");
-	opt.addUsage(" -g  --geometry       <path_to_acceptance_ROOT_file>      (*) Acceptance file - flux calculation only");
-	opt.addUsage(" -v  --verbose                                                Verbose output");
-	opt.addUsage(" -p  --pedantic                                               Pedantic output");
+	opt.addUsage(" -h  --help                                                   		Prints this help");
+	opt.addUsage(" -i  --input          <path_to_input_DATA_list>           		(*) Input DATA list");
+	opt.addUsage(" -w  --workdir        <path_to_software_config_dir>       		(*) Config directory");
+	opt.addUsage(" -o  --output         <path_to_output_TFile>                          Output ROOT TFile");
+	opt.addUsage(" -d  --outputDir      <path_to_output_TFile_dir>                      Output ROOT TFile directory");
+	opt.addUsage(" -t  --lvtime         <live_time-value>                   		(*) DAMPE live-time ");
+	opt.addUsage(" -a  --acceptance     <path_to_MC_list>                   		(*) Acceptance calculation");
+	opt.addUsage(" -n  --ntuple                                             		(*) nTuple production facility");
+	opt.addUsage(" -c  --collect        <path_to_complete_histo>            		(*) Generate TGraph from final histo");
+	opt.addUsage(" -r  --resume         <mc/data>                           		(*) MC/DATA facility");
+	opt.addUsage(" -g  --geometry       <path_to_electron_acceptance_ROOT_file>     (*) Electron Acceptance file - flux calculation only");
+	opt.addUsage(" -b  --background     <path_to_proton_acceptance_ROOT_file>       (*) Proton Acceptance file - flux calculation only");
+	opt.addUsage(" -v  --verbose                                                        Verbose output");
+	opt.addUsage(" -p  --pedantic                                                       Pedantic output");
 	opt.addUsage("");
 
 	opt.setFlag("help", 'h');
@@ -33,6 +34,7 @@ int main(int argc, char **argv)
 	opt.setOption("lvtime", 't');
 	opt.setOption("acceptance", 'a');
 	opt.setOption("geometry", 'g');
+	opt.setOption("background", 'b');
 	opt.setOption("collect", 'c');
 	opt.setOption("resume", 'r');
 	opt.setFlag("verbose", 'v');
@@ -50,6 +52,7 @@ int main(int argc, char **argv)
 	std::string inputPath;
 	std::string outputPath;
 	std::string accInputPath;
+	std::string p_accInputPath;
 	std::string inputCompleteHisto;
 	stringstream str_lvTime;
 
@@ -93,6 +96,8 @@ int main(int argc, char **argv)
 		myNtuple = true;
 	if (opt.getValue("geometry") || opt.getValue('g'))
 		accInputPath = opt.getValue('g');
+	if (opt.getValue("background") || opt.getValue('b'))
+		p_accInputPath = opt.getValue('b');
 	if (opt.getFlag("verbose") || opt.getFlag('v'))
 		verbose = opt.getFlag('v');
 	if (opt.getValue("collect") || opt.getValue('c'))
@@ -152,6 +157,7 @@ int main(int argc, char **argv)
 			pedantic,
 			lvTime,
 			accInputPath,
+			p_accInputPath,
 			opt,
 			wd);
 
