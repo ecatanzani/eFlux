efb357d03797bf39a12b52c1498d6be590810a81
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-09-07 17:44:52 +0200

    Major refactory

--- Assets/lvTimeCounter.py
+++ Assets/lvTimeCounter.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
-from ROOT import * 
+from ROOT import *
+from argparse import ArgumentParser
+
 
 def main(args=None):
-    gSystem.Load("libDmpService.so") 
-    a=DmpSvcLiveTime.GetInstance() 
-    #01/01/16-31/10/17 
-    b =a.GetLiveTime(94608354,152496303); 
-    
-if __name__ == '__main__' :
+    parser = ArgumentParser(
+        usage="Usage: %(prog)s [options]", description="DAMPE live-time facility")
+    parser.add_argument("-s", "--start", type=int, dest='start_time',
+                        const=94608354, nargs='?', help='start time')
+    parser.add_argument("-e", "--end", type=int, dest='end_time',
+                        const=238981295, nargs='?', help='end time')
+    opts = parser.parse_args(args)
+
+    gSystem.Load("libDmpService.so")
+    _time_instance = DmpSvcLiveTime.GetInstance()
+    live_time = _time_instance.GetLiveTime(opts.start_time, opts.end_time)
+    print('Live-Time: {}'.format(live_time))
+
+
+if __name__ == '__main__':
     main()
