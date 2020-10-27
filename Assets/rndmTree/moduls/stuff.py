1d246dcd4b861630047ad2aafa2cd9f88cb06381
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2020-10-27 15:12:44 +0100

    Deprecated

--- Assets/rndmTree/moduls/stuff.py
+++
@@ -1,4 +0,0 @@
-def eventProcess(opts,evIdx,totEvents,kStep=1):
-    if opts.verbose:
-        if (evIdx%kStep)==0 and evIdx!=0:
-            print('\tProcessed event {} of {}'.format(evIdx,totEvents))
