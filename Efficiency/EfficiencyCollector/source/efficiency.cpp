fc85ced263f0b92710b3a1e7b3c681067409fe07
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-01-25 12:18:32 +0100

    Updated including local energy config file parsing

--- Efficiency/EfficiencyCollector/source/efficiency.cpp
+++ Efficiency/EfficiencyCollector/source/efficiency.cpp
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include "config.h"
 #include "efficiency.h"
+#include "energy_config.h"
 
 #include "TFile.h"
 #include "TChain.h"
@@ -8,17 +9,23 @@
 
 void BuildEfficiencyHistos(in_pars input_pars)
 {
+    // Parse input file list
     std::unique_ptr<parser> evt_parser = std::make_unique<parser>(
         input_pars.input_path,
         input_pars.mc_flag,
         input_pars.verbose);
+    // Parse 'Collector' config file
     std::unique_ptr<config> _config = std::make_unique<config>(
         input_pars.wd, 
         input_pars.mc_flag);
+    // Parse local config file
+    std::shared_ptr<energy_config> _energy_config = std::make_shared<energy_config>(input_pars.wd);
+    // Get chain entries
     const double _entries = evt_parser->GetEvtTree()->GetEntries();
     if (input_pars.verbose)
     {
         _config->PrintActiveFilters();
+        _energy_config->PrintActiveFilters();
         std::cout << "Total number of events: " << _entries;
     }
 
