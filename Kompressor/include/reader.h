8545a11108dcd8b5f29dad0afb46ca2a17523af9
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-03-10 10:44:50 +0100

    headers updated

--- Kompressor/include/reader.h
+++ Kompressor/include/reader.h
@@ -1,27 +1,8 @@
 #ifndef READER_H
 #define READER_H
 
-#include <memory>
-#include <string>
-#include <memory>
-
-#include "TChain.h" 
-
 #include "main.h"
-#include "config.h"
-#include "energy_config.h"
 
 extern void reader(in_args input_args);
-
-extern void kompress(
-    std::shared_ptr<TChain> evtch,
-    std::shared_ptr<config> _config,
-    std::shared_ptr<energy_config> _energy_config,
-    const std::string fit_tree_path,
-    const double _entries,
-    const std::string outputPath,
-    const bool _VERBOSE,
-    const unsigned int threads,
-    const bool _mc);
 
 #endif
