ab982f90ddf18a48a7ec5e614e42c55c69ae9e63
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-03-10 21:01:59 +0100

    Updated with 'show_gaussianized' function

--- Kompressor/include/gaussianize.h
+++ Kompressor/include/gaussianize.h
@@ -23,5 +23,22 @@ extern void gaussianizeTMVAvars(
     const unsigned int threads,
     const bool _mc);
 
+extern void showGaussianizedTMVAvars(
+    std::shared_ptr<TChain> evtch,
+    std::shared_ptr<config> _config,
+    std::shared_ptr<energy_config> _energy_config,
+    std::shared_ptr<lambda_config> _lambda_config,
+    const double _entries,
+    const std::string outputPath,
+    const bool _VERBOSE,
+    const unsigned int threads,
+    const bool _mc);
+
+extern std::vector<std::vector<std::vector<std::shared_ptr<TH1D>>>> GetRMSLayerHistos(
+    const int energy_nbins, 
+    const lambdas lambda_values,
+    const int DAMPE_bgo_nLayers);
+
 extern void ComputeGoodness(std::vector<std::shared_ptr<TH1D>> h_rms);
+
 #endif
