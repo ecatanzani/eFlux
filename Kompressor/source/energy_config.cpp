0eebb46ab8158d2579deac7bc1ddf42cfe879fee
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-03-10 10:45:00 +0100

    minor modifications

--- Kompressor/source/energy_config.cpp
+++ Kompressor/source/energy_config.cpp
@@ -5,8 +5,8 @@ energy_config::energy_config(const std::string working_dir)
 {
     const std::string local_path = "/Kompressor/config";
     const std::string config_file_name = "energy_config.conf";
-    const auto kmp_config = working_dir.substr(0, working_dir.find("/Collector/config")) + local_path;
-    get_config_info(parse_config_file(kmp_config, config_file_name));
+    const auto tmp_config = working_dir.substr(0, working_dir.find("/Collector/config")) + local_path;
+    get_config_info(parse_config_file(tmp_config, config_file_name));
     energy_binning = createLogBinning(
 		min_event_energy,
 		max_event_energy,
