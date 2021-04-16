81cc94a4c3dd32188ae49a451f87547f1596c06d
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-04-16 15:43:40 +0200

    Major update including XROOTD paths

--- Collector/testDATA.py
+++ Collector/testDATA.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from ROOT import TFile
 
 def TestROOTFile(path: str = "") -> bool:
-    _tmp_file = TFile(path)
+    _tmp_file = TFile.Open(path)
     if _tmp_file and not _tmp_file.IsOpen():
         return False
     elif _tmp_file and _tmp_file.IsOpen() and _tmp_file.IsZombie():
@@ -18,14 +18,26 @@ def TestROOTFile(path: str = "") -> bool:
         return True
 
 def parseDataFiles(in_file: str = "", verbose: bool = False):
+    TGREEN =  '\033[32m'
+    TRED =  '\033[41m'
+    ENDC = '\033[m'
+    COLOR = TRED
     zombie_files = []
+    good_files = []
     with open(in_file, "r") as _file:
         lines = _file.read().splitlines() 
     for line in lines:
-        if verbose:
-            print(f"Testing {line}")
-        if not TestROOTFile(line):
+        file_status = TestROOTFile(line)
+        status_exp = ""
+        if file_status:
+            good_files.append(line)
+            COLOR = TGREEN
+            status_exp = "OK"
+        else:
             zombie_files.append(line)
+            status_exp = "ZOMBIE"
+        if verbose:
+            print(f"File {line} --> {COLOR}{status_exp}", ENDC)
             
     if zombie_files:
         print(f"\nHere the ZOMBIE data file list ({len(zombie_files)} elements / {len(lines)}):\n")
@@ -33,7 +45,6 @@ def parseDataFiles(in_file: str = "", verbose: bool = False):
             for zfile in zombie_files:
                 print(zfile)
                 zoutfile.write(f"{zfile}\n")
-            
 
 def main(args=None):
     parser = ArgumentParser(
