91812bea488a3f59cf4b3dd586c5090427844178
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-05-06 18:40:13 +0200

    Major bug fix in both data reading and plot config

--- Assets/SkimmedStatisticsPlot/moduls/buildHisto.py
+++ Assets/SkimmedStatisticsPlot/moduls/buildHisto.py
@@ -4,6 +4,7 @@ import numpy as np
 from tqdm import tqdm
 from ROOT import TFile, TTree
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from datetime import date, timedelta
 
 def testROOTFile(path: str) -> bool:
@@ -105,6 +106,21 @@ def fillStats(files: list, pars: dict, opts: argparse.Namespace) -> dict:
                 tmpstats[erange] = -1
             timebins.append(sdate)
             sdate = tmpdate
+
+            if testROOTFile(rootfilename):
+                filestats = getStatsFromFile(rootfilename)
+                if "002_010" in file:
+                    tmpstats['20_100'] = filestats
+                elif "010_025" in file:
+                    tmpstats['100_250'] = filestats
+                elif "025_050" in file:
+                    tmpstats['250_500'] = filestats
+                elif "050_100" in file:
+                    tmpstats['500_1'] = filestats
+                elif "100_500" in file:
+                    tmpstats['1_5'] = filestats
+                elif "500_000" in file:
+                    tmpstats['g5'] = filestats
     
     return {'timebins': timebins, 'evts': evts, 'evts_20_100': evts_20_100, 'evts_100_250': evts_100_250, 'evts_250_500': evts_250_500, 'evts_500_1': evts_500_1, 'evts_1_5': evts_1_5, 'evts_5': evts_5}
 
@@ -113,17 +129,26 @@ def buildHisto(opts: argparse.Namespace, files: list, pars: dict):
     stats = fillStats(files, pars, opts)
 
     # Plot
-    plt.style.use('seaborn-whitegrid')
+    '''
     plt.plot(stats['timebins'], stats['evts'], marker='p', label="all energy", color="dimgray", linestyle='dashed', linewidth=2, markersize=2)
-    plt.plot(stats['timebins'], stats['evts_20_100'], marker='o', label="20 - 100 GeV", color="cornflowerblue", linestyle='dashed', linewidth=2, markersize=2)
-    plt.plot(stats['timebins'], stats['evts_100_250'], marker='^', label="100 - 250 GeV", color="darkorange", linestyle='dashed', linewidth=2, markersize=2)
-    plt.plot(stats['timebins'], stats['evts_250_500'], marker='*', label="250 - 500 GeV", color="forestgreen", linestyle='dashed', linewidth=2, markersize=2)
-    plt.plot(stats['timebins'], stats['evts_500_1'], marker='|', label="0.5 - 1 TeV", color="crimson", linestyle='dashed', linewidth=2, markersize=2)
-    plt.plot(stats['timebins'], stats['evts_1_5'], marker='d', label="1 - 5 TeV", color="blueviolet", linestyle='dashed', linewidth=2, markersize=2)
-    plt.plot(stats['timebins'], stats['evts_5'], marker='s', label="> 5 TeV", color="saddlebrown", linestyle='dashed', linewidth=2, markersize=2)
-
-    #plt.legend(numpoints=1)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., numpoints=1)
+    plt.plot(np.array(stats['timebins']), np.array(stats['evts_20_100']), marker='o', label="20 - 100 GeV", color="cornflowerblue", linestyle='dashed', linewidth=2, markersize=2)
+    plt.plot(np.array(stats['timebins']), np.array(stats['evts_100_250']), marker='^', label="100 - 250 GeV", color="darkorange", linestyle='dashed', linewidth=2, markersize=2)
+    plt.plot(np.array(stats['timebins']), np.array(stats['evts_250_500']), marker='*', label="250 - 500 GeV", color="forestgreen", linestyle='dashed', linewidth=2, markersize=2)
+    plt.plot(np.array(stats['timebins']), np.array(stats['evts_500_1']), marker='|', label="0.5 - 1 TeV", color="crimson", linestyle='dashed', linewidth=2, markersize=2)
+    plt.plot(np.array(stats['timebins']), np.array(stats['evts_1_5']), marker='d', label="1 - 5 TeV", color="blueviolet", linestyle='dashed', linewidth=2, markersize=2)
+    plt.plot(np.array(stats['timebins']), np.array(stats['evts_5']), marker='s', label="> 5 TeV", color="saddlebrown", linestyle='dashed', linewidth=2, markersize=2)
+    '''
+    
+    rcParams.update({'figure.autolayout': True})
+    plt.plot(stats['timebins'], stats['evts'], label="all energy", color="dimgray")
+    plt.plot(stats['timebins'], stats['evts_20_100'], label="20 - 100 GeV", color="cornflowerblue")
+    plt.plot(stats['timebins'], stats['evts_100_250'], label="100 - 250 GeV", color="darkorange")
+    plt.plot(stats['timebins'], stats['evts_250_500'], label="250 - 500 GeV", color="forestgreen")
+    plt.plot(stats['timebins'], stats['evts_500_1'], label="0.5 - 1 TeV", color="crimson")
+    plt.plot(stats['timebins'], stats['evts_1_5'], label="1 - 5 TeV", color="blueviolet")
+    plt.plot(stats['timebins'], stats['evts_5'], label="> 5 TeV", color="saddlebrown")
+    
+    plt.legend(bbox_to_anchor=(1.05, 0.5), loc='center left')
     plt.yscale('log')
     plt.ylim(10, 1e+6)
     plt.ylabel("counts")
