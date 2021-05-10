daeb2d0a0c807e054f4bf9a10021df2948bd7f51
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-05-10 14:42:40 +0200

    All energy bins are equal

--- Kompressor/source/gaus_support.cpp
+++ Kompressor/source/gaus_support.cpp
@@ -18,7 +18,7 @@ void extract_lamda_info(
         auto xmin = input_histo[bin_idx-1][lambda_idx][layer]->GetXaxis()->GetXmin();
         auto xmax = input_histo[bin_idx-1][lambda_idx][layer]->GetXaxis()->GetXmax();
         std::unique_ptr<TF1> fitfunc = std::make_unique<TF1>("fitfunc", "gaus", xmin, xmax);
-        input_histo[bin_idx-1][lambda_idx][layer]->Fit("fitfunc", "Q");
+        input_histo[bin_idx-1][lambda_idx][layer]->Fit("fitfunc", "QW");
         auto chi2 = fitfunc->GetChisquare();
         auto dof = fitfunc->GetNDF();
         
