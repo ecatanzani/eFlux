4299cb648a0477cbded62900b9969cec5380c31a
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-10-21 15:37:35 +0200

    Minor modifications + MC/DATA facility has been divided
    Data facility has not been completed yet

--- Collector/assets/PreSelection/include/bgofiducial.h
+++ Collector/assets/PreSelection/include/bgofiducial.h
@@ -13,6 +13,21 @@ extern void bgofiducial_distributions(
     const std::string logs_dir, 
     std::shared_ptr<TChain> evtch,
     std::shared_ptr<config> evt_config,
+    const bool mc,
+    const bool verbose);
+
+extern void bgofiducial_distributions_mc(
+    const std::string output_path, 
+    const std::string logs_dir,
+    std::shared_ptr<TChain> evtch,
+    std::shared_ptr<config> evt_config,
+    const bool verbose);
+
+extern void bgofiducial_distributions_data(
+    const std::string output_path, 
+    const std::string logs_dir,
+    std::shared_ptr<TChain> evtch,
+    std::shared_ptr<config> evt_config,
     const bool verbose);
 
 #endif
--- Collector/assets/PreSelection/source/preselection.cpp
+++ Collector/assets/PreSelection/source/preselection.cpp
@@ -13,6 +13,6 @@ void preselection(const in_pars &input_pars) {
     auto evtch = GetFileChain(input_pars.input_path, input_pars.verbose);
     std::shared_ptr<config> evt_config = std::make_shared<config>(input_pars.config_wd);
 
-    bgofiducial_distributions(input_pars.output_path, input_pars.logs_dir, evtch, evt_config, input_pars.verbose);
+    bgofiducial_distributions(input_pars.output_path, input_pars.logs_dir, evtch, evt_config, input_pars.mc, input_pars.verbose);
 
 }
