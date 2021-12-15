9a11681858be819519bf7a8541a599f20a04548b
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-12-15 18:03:18 +0100

    Major bug fix + minor code refactory
    data structs have been moved to bdtinfo header

--- Classifier/Reader/include/bdtinfo.h
+++ Classifier/Reader/include/bdtinfo.h
@@ -3,6 +3,78 @@
 
 #include "main.h"
 
+struct bdt_vars {
+    float rmslayer_norm_1 {0};
+    float rmslayer_norm_2 {0};
+    float rmslayer_norm_3 {0};
+    float rmslayer_norm_4 {0};
+    float rmslayer_norm_5 {0};
+    float rmslayer_norm_6 {0};
+    float rmslayer_norm_7 {0};
+    float rmslayer_norm_8 {0};
+    float rmslayer_norm_9 {0};
+    float rmslayer_norm_10 {0};
+    float rmslayer_norm_11 {0};
+    float rmslayer_norm_12 {0};
+    float rmslayer_norm_13 {0};
+    float rmslayer_norm_14 {0};
+    float fraclayer_norm_1 {0};
+    float fraclayer_norm_2 {0};
+    float fraclayer_norm_3 {0};
+    float fraclayer_norm_4 {0};
+    float fraclayer_norm_5 {0};
+    float fraclayer_norm_6 {0};
+    float fraclayer_norm_7 {0};
+    float fraclayer_norm_8 {0};
+    float fraclayer_norm_9 {0};
+    float fraclayer_norm_10 {0};
+    float fraclayer_norm_11 {0};
+    float fraclayer_norm_12 {0};
+    float fraclayer_norm_13 {0};
+    float fraclayer_norm_14 {0};
+    float sumrms_norm {0};
+    float fraclastlayer_norm {0};
+    float xtrl_norm {0};
+    float xtrl {0};
+};
+
+struct data_vars {
+    double rmslayer_norm_1 {0};
+    double rmslayer_norm_2 {0};
+    double rmslayer_norm_3 {0};
+    double rmslayer_norm_4 {0};
+    double rmslayer_norm_5 {0};
+    double rmslayer_norm_6 {0};
+    double rmslayer_norm_7 {0};
+    double rmslayer_norm_8 {0};
+    double rmslayer_norm_9 {0};
+    double rmslayer_norm_10 {0};
+    double rmslayer_norm_11 {0};
+    double rmslayer_norm_12 {0};
+    double rmslayer_norm_13 {0};
+    double rmslayer_norm_14 {0};
+    double fraclayer_norm_1 {0};
+    double fraclayer_norm_2 {0};
+    double fraclayer_norm_3 {0};
+    double fraclayer_norm_4 {0};
+    double fraclayer_norm_5 {0};
+    double fraclayer_norm_6 {0};
+    double fraclayer_norm_7 {0};
+    double fraclayer_norm_8 {0};
+    double fraclayer_norm_9 {0};
+    double fraclayer_norm_10 {0};
+    double fraclayer_norm_11 {0};
+    double fraclayer_norm_12 {0};
+    double fraclayer_norm_13 {0};
+    double fraclayer_norm_14 {0};
+    double sumrms_norm {0};
+    double fraclastlayer_norm {0};
+    double xtrl_norm {0};
+    double xtrl {0};
+
+    double evt_corr_energy {0};
+};
+
 extern void ExtractBDTInfo(in_args input_args);
 
 #endif
--- Classifier/Reader/source/bdtinfo.cpp
+++ Classifier/Reader/source/bdtinfo.cpp
@@ -10,41 +10,6 @@
 #include "TMVA/Tools.h"
 #include "TMVA/Reader.h"
 
-struct bdt_vars {
-    float rmslayer_norm_1 {0};
-    float rmslayer_norm_2 {0};
-    float rmslayer_norm_3 {0};
-    float rmslayer_norm_4 {0};
-    float rmslayer_norm_5 {0};
-    float rmslayer_norm_6 {0};
-    float rmslayer_norm_7 {0};
-    float rmslayer_norm_8 {0};
-    float rmslayer_norm_9 {0};
-    float rmslayer_norm_10 {0};
-    float rmslayer_norm_11 {0};
-    float rmslayer_norm_12 {0};
-    float rmslayer_norm_13 {0};
-    float rmslayer_norm_14 {0};
-    float fraclayer_norm_1 {0};
-    float fraclayer_norm_2 {0};
-    float fraclayer_norm_3 {0};
-    float fraclayer_norm_4 {0};
-    float fraclayer_norm_5 {0};
-    float fraclayer_norm_6 {0};
-    float fraclayer_norm_7 {0};
-    float fraclayer_norm_8 {0};
-    float fraclayer_norm_9 {0};
-    float fraclayer_norm_10 {0};
-    float fraclayer_norm_11 {0};
-    float fraclayer_norm_12 {0};
-    float fraclayer_norm_13 {0};
-    float fraclayer_norm_14 {0};
-    float sumrms_norm {0};
-    float fraclastlayer_norm {0};
-    float xtrl_norm {0};
-    float xtrl {0};
-};
-
 inline void addVariableToReader(std::shared_ptr<TMVA::Reader> tmva_reader, bdt_vars &tmva_vars) {
     tmva_reader->AddVariable("rmslayer_norm_1", &tmva_vars.rmslayer_norm_1);
     tmva_reader->AddVariable("rmslayer_norm_2", &tmva_vars.rmslayer_norm_2);
@@ -82,7 +47,7 @@ inline void addVariableToReader(std::shared_ptr<TMVA::Reader> tmva_reader, bdt_v
     tmva_reader->AddSpectator("xtrl", &tmva_vars.xtrl);
 }
 
-inline void linkTreeVariables(std::shared_ptr<TChain> evtch, bdt_vars &vars, double &evt_energy) {
+inline void linkTreeVariables(std::shared_ptr<TChain> evtch, data_vars &vars) {
     evtch->SetBranchAddress("rmslayer_norm_1", &vars.rmslayer_norm_1);
     evtch->SetBranchAddress("rmslayer_norm_2", &vars.rmslayer_norm_2);
     evtch->SetBranchAddress("rmslayer_norm_3", &vars.rmslayer_norm_3);
@@ -118,8 +83,45 @@ inline void linkTreeVariables(std::shared_ptr<TChain> evtch, bdt_vars &vars, dou
     evtch->SetBranchAddress("xtrl_norm", &vars.xtrl_norm);
     evtch->SetBranchAddress("xtrl", &vars.xtrl);
 
-    evtch->SetBranchAddress("energy_corr", &evt_energy);
+    evtch->SetBranchAddress("energy_corr", &vars.evt_corr_energy);
 
+}
+
+inline void sync_vars(const data_vars &vars, bdt_vars &tmva_vars) {
+    tmva_vars.rmslayer_norm_1 = vars.rmslayer_norm_1;
+    tmva_vars.rmslayer_norm_2 = vars.rmslayer_norm_2;
+    tmva_vars.rmslayer_norm_3 = vars.rmslayer_norm_3;
+    tmva_vars.rmslayer_norm_4 = vars.rmslayer_norm_4;
+    tmva_vars.rmslayer_norm_5 = vars.rmslayer_norm_5;
+    tmva_vars.rmslayer_norm_6 = vars.rmslayer_norm_6;
+    tmva_vars.rmslayer_norm_7 = vars.rmslayer_norm_7;
+    tmva_vars.rmslayer_norm_8 = vars.rmslayer_norm_8;
+    tmva_vars.rmslayer_norm_9 = vars.rmslayer_norm_9;
+    tmva_vars.rmslayer_norm_10 = vars.rmslayer_norm_10;
+    tmva_vars.rmslayer_norm_11 = vars.rmslayer_norm_11;
+    tmva_vars.rmslayer_norm_12 = vars.rmslayer_norm_12;
+    tmva_vars.rmslayer_norm_13 = vars.rmslayer_norm_13;
+    tmva_vars.rmslayer_norm_14 = vars.rmslayer_norm_14;
+
+    tmva_vars.fraclayer_norm_1 = vars.fraclayer_norm_1;
+    tmva_vars.fraclayer_norm_2 = vars.fraclayer_norm_2;
+    tmva_vars.fraclayer_norm_3 = vars.fraclayer_norm_3;
+    tmva_vars.fraclayer_norm_4 = vars.fraclayer_norm_4;
+    tmva_vars.fraclayer_norm_5 = vars.fraclayer_norm_5;
+    tmva_vars.fraclayer_norm_6 = vars.fraclayer_norm_6;
+    tmva_vars.fraclayer_norm_7 = vars.fraclayer_norm_7;
+    tmva_vars.fraclayer_norm_8 = vars.fraclayer_norm_8;
+    tmva_vars.fraclayer_norm_9 = vars.fraclayer_norm_9;
+    tmva_vars.fraclayer_norm_10 = vars.fraclayer_norm_10;
+    tmva_vars.fraclayer_norm_11 = vars.fraclayer_norm_11;
+    tmva_vars.fraclayer_norm_12 = vars.fraclayer_norm_12;
+    tmva_vars.fraclayer_norm_13 = vars.fraclayer_norm_13;
+    tmva_vars.fraclayer_norm_14 = vars.fraclayer_norm_14;
+
+    tmva_vars.sumrms_norm = vars.sumrms_norm;
+    tmva_vars.fraclastlayer_norm = vars.fraclastlayer_norm;
+    tmva_vars.xtrl_norm = vars.xtrl_norm;
+    tmva_vars.xtrl = vars.xtrl;
 }
 
 void ExtractBDTInfo(in_args input_args)
@@ -142,6 +144,7 @@ void ExtractBDTInfo(in_args input_args)
 
     // Declare BDT variables
     bdt_vars tmva_vars;
+    data_vars vars;
 
     // Attach avriables to reader
     addVariableToReader(tmva_LE_reader, tmva_vars);
@@ -151,9 +154,8 @@ void ExtractBDTInfo(in_args input_args)
     tmva_LE_reader->BookMVA(input_args.learning_method.c_str(), (sw_config->GetLEWeights()).c_str());
     tmva_ME_reader->BookMVA(input_args.learning_method.c_str(), (sw_config->GetMEWeights()).c_str());
     tmva_HE_reader->BookMVA(input_args.learning_method.c_str(), (sw_config->GetHEWeights()).c_str());
-
-    double event_energy;
-    linkTreeVariables(list_parser->GetEvtTree(), tmva_vars, event_energy);
+    
+    linkTreeVariables(list_parser->GetEvtTree(), vars);
 
     // Clone TChain
     std::shared_ptr<TTree> electron_tree = std::shared_ptr<TTree>(static_cast<TTree*>(list_parser->GetEvtTree()->CloneTree(0)));
@@ -164,19 +166,20 @@ void ExtractBDTInfo(in_args input_args)
     bool is_electron;
     for (unsigned int evidx=0; evidx<list_parser->GetEvtTree()->GetEntries(); ++evidx) {
         list_parser->GetEvtTree()->GetEntry(evidx);
+        sync_vars(vars, tmva_vars);
         is_electron =false;
 
-        if (event_energy*gev>=10 && event_energy*gev<100) {
+        if (vars.evt_corr_energy*gev>=10 && vars.evt_corr_energy*gev<100) {
             tmva_classifier = tmva_LE_reader->EvaluateMVA(input_args.learning_method.c_str());
             if (tmva_classifier>sw_config->GetLEClassifierCut())
                 is_electron = true;
         }
-        else if (event_energy*gev>=100 && event_energy*gev<1000) {
+        else if (vars.evt_corr_energy*gev>=100 && vars.evt_corr_energy*gev<1000) {
             tmva_classifier = tmva_ME_reader->EvaluateMVA(input_args.learning_method.c_str());
             if (tmva_classifier>sw_config->GetMEClassifierCut())
                 is_electron = true;
         }
-        else if (event_energy*gev>=1000 && event_energy*gev<10000) {
+        else if (vars.evt_corr_energy*gev>=1000 && vars.evt_corr_energy*gev<10000) {
             tmva_classifier = tmva_HE_reader->EvaluateMVA(input_args.learning_method.c_str());
             if (tmva_classifier>sw_config->GetHEClassifierCut())
                 is_electron = true;
