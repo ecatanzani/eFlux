8a2bd2b2fdfff6927816ed6fff2337411a7e6b1b
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-12-16 16:22:16 +0100

    Major modifications
    Timestamps are now automatically computed

--- assets/lvTimeCounter.py
+++ assets/lvTimeCounter.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
-from ROOT import *
+import time
+import datetime
+from ROOT import gSystem
 from argparse import ArgumentParser
 
+def data_to_timestamp(data):
+    return int(time.mktime(datetime.datetime.strptime(data, "%d/%m/%Y").timetuple()))
 
 def main(args=None):
-    parser = ArgumentParser(
-        usage="Usage: %(prog)s [options]", description="DAMPE live-time facility")
-    parser.add_argument("-s", "--start", type=int, dest='start_time',
-                        const=94608354, nargs='?', help='start time')
-    parser.add_argument("-e", "--end", type=int, dest='end_time',
-                        const=238981295, nargs='?', help='end time')
+    parser = ArgumentParser(usage="Usage: %(prog)s [options]", description="DAMPE live-time facility")
+    parser.add_argument("-s", "--start", type=str, dest='start_date', help='start date (dd/mm/yyyy)')
+    parser.add_argument("-e", "--end", type=str, dest='end_date', help='start date (dd/mm/yyyy)')
+    parser.add_argument("-v", "--verbose", dest='verbose', default=False, action='store_true', help='run in high verbosity mode')
+    
     opts = parser.parse_args(args)
 
+    if opts.start_date is None:
+        opts.start_date = "01/01/2016"
+        if opts.verbose:
+            print('The start date has not been provided... default one is used[{}]'.format(opts.start_date))
+    if opts.end_date is None:
+        opts.end_date = "03/11/2021"
+        if opts.verbose:
+            print('The end date has not been provided... default one is used[{}]'.format(opts.end_date))
+
+    if (opts.verbose):
+        print('Computing LiveTime in the following interval: [{}, {}]'.format(opts.start_date, opts.end_date))
     gSystem.Load("libDmpService.so")
-    _time_instance = DmpSvcLiveTime.GetInstance()
-    live_time = _time_instance.GetLiveTime(opts.start_time, opts.end_time)
-    print('Live-Time: {}'.format(live_time))
+    _time_instance = DmpSvcLiveTime.GetInstance(data_to_timestamp(opts.start_date), data_to_timestamp(opts.end_date))
+    live_time = _time_instance.GetLiveTime()
+    
+    print('LiveTime: {}'.format(live_time))
 
 
 if __name__ == '__main__':
