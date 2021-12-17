3e1d3a726f8ea00aadd20a5757b6dec806b3e632
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-12-17 10:06:38 +0100

    Major bug fix
    Code has been tested and works correctly

--- assets/lvTimeCounter.py
+++ assets/lvTimeCounter.py
@@ -1,37 +1,44 @@
 #!/usr/bin/env python
 import time
 import datetime
-from ROOT import *
+import ROOT
 from argparse import ArgumentParser
 
 def data_to_timestamp(data):
     return int(time.mktime(datetime.datetime.strptime(data, "%d/%m/%Y").timetuple()))
 
-def compute_livetime(start_date, end_date):
-    gSystem.Load("libDmpService.so")
-    return DmpSvcLiveTime.GetInstance().GetLiveTime(data_to_timestamp(start_date), data_to_timestamp(end_date))
+def compute_livetime(start_sec, end_sec):
+    return ROOT.DmpSvcLiveTime.GetInstance().GetLiveTime(start_sec, end_sec)
+
+def extract_seconds_from_files(files, verbose):
+    evtch = ROOT.DmpChain("CollectionTree")
+    for single_file in files:
+        evtch.Add(single_file)
+        if verbose:
+            print('Adding {} to the chain...'.format(single_file))
+    
+    seconds = []
+    for ev_idx in range(evtch.GetEntries()):
+        pev = evtch.GetDmpEvent(ev_idx)
+        evthdr = pev.pEvtHeader()
+        seconds.append(int(evthdr.GetSecond()))
+    
+    return min(seconds), max(seconds)
 
 def main(args=None):
     parser = ArgumentParser(usage="Usage: %(prog)s [options]", description="DAMPE live-time facility")
-    parser.add_argument("-s", "--start", type=str, dest='start_date', help='start date (dd/mm/yyyy)')
-    parser.add_argument("-e", "--end", type=str, dest='end_date', help='start date (dd/mm/yyyy)')
+    parser.add_argument("-s", "--start", type=str, dest='start_file', help='start 2A DAMPE data file')
+    parser.add_argument("-e", "--end", type=str, dest='end_file', help='end 2A DAMPE data file')
     parser.add_argument("-v", "--verbose", dest='verbose', default=False, action='store_true', help='run in high verbosity mode')
     
     opts = parser.parse_args(args)
-
-    if opts.start_date is None:
-        opts.start_date = "01/01/2016"
-        if opts.verbose:
-            print('\nThe start date has not been provided... default one is used[{}]'.format(opts.start_date))
-    if opts.end_date is None:
-        opts.end_date = "03/11/2021"
-        if opts.verbose:
-            print('\nThe end date has not been provided... default one is used[{}]'.format(opts.end_date))
+    ROOT.gSystem.Load("libDmpService.so")
+    start_second, end_second = extract_seconds_from_files((opts.start_file, opts.end_file), opts.verbose)
 
     if (opts.verbose):
-        print('\n\nComputing LiveTime in the following interval: [{}, {}]\n\n'.format(opts.start_date, opts.end_date))
+        print('\nComputing LiveTime in the following interval: [{}, {}]\n'.format(start_second, end_second))
     
-    print('LiveTime: {}'.format(compute_livetime(opts.start_date, opts.end_date)))
+    print('LiveTime: {}'.format(compute_livetime(start_second, end_second)))
 
 
 if __name__ == '__main__':
