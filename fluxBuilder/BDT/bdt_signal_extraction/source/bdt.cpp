e90482ecefca6d42b9aec657a05e7f3031d08367
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-01-29 19:09:07 +0100

    Signal extraction utility has been added

--- fluxBuilder/BDT/bdt_signal_extraction/source/bdt.cpp
+++ fluxBuilder/BDT/bdt_signal_extraction/source/bdt.cpp
@@ -12,9 +12,6 @@ void bdt_selection(in_args input_args) {
     std::shared_ptr<energy_config> config = std::make_shared<energy_config>(input_args.energy_config_file);
     std::shared_ptr<parser> evt_parser = std::make_unique<parser>(input_args.input_list, input_args.verbose);
 
-    auto energy_binning = config->GetEnergyBinning();
-    auto energy_nbins = (int)energy_binning.size() - 1;
-
     ROOT::EnableImplicitMT(input_args.threads);
     ROOT::RDataFrame _data_fr(*evt_parser->GetEvtTree());
 
@@ -25,17 +22,5 @@ void bdt_selection(in_args input_args) {
 
     if (input_args.verbose) std::cout << "\n\nAnlysis running...\n\n";
 
-    auto h_bdt_selection = _data_fr.Define("corr_energy_gev", "energy_corr * 0.001")
-                                    .Filter("xtrl<10")
-                                    .Histo1D<double>({"h_bdt_selection", "BDT selected events; BGO Corr energy [GeV]; entries", energy_nbins, &energy_binning[0]}, "corr_energy_gev");
-
-    TFile* outfile = TFile::Open(input_args.output_path.c_str(), "RECREATE");
-    if (!outfile->IsOpen()) {
-        std::cerr << "\n\nError writing output file... [" << input_args.output_path << "]\n\n";
-        exit(100);
-    }
-
-    h_bdt_selection->Write();
-
-    outfile->Close();
+    _data_fr.Snapshot(evt_parser->GetEvtTree()->GetName(), input_args.output_path);
 }
