7303dcbfee0dc5fd3356f49d38b832815153a3c5
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-01-30 22:59:11 +0100

    Major modifications to allCRs plot

--- assets/Plots/allCRs/plotutils.cpp
+++ assets/Plots/allCRs/plotutils.cpp
@@ -21,19 +21,29 @@
 const bool AMS02_proton = true;
 const bool CALET_proton = true;
 const bool DAMPE_proton = true;
-const bool DAMPE_electron = true;
+const bool DAMPE_electron = false;
 const bool AMS02_electron = true;
-const bool CALET_electron = true;
-const bool FERMILAT_electron = true;
-const bool AUGER_allparticle = true;
-const bool KASCADEgrande_allparticle = true;
-const bool ATIC2_proton = true;
+const bool AMS02_positron = true;
+const bool AMS02_pbar = true;
+const bool AMS02_B = true;
+const bool AMS02_C = true;
+const bool AMS02_O = true;
+const bool CALET_electron = false;
+const bool FERMILAT_electron = false;
+const bool AUGER_allparticle = false;
+const bool KASCADEgrande_allparticle = false;
+const bool ATIC2_proton = false;
 const bool AMS02_helium = true;
 
 TColor *kAMS02_proton = gROOT->GetColor(kRed+3);
 TColor *kCALET_proton = gROOT->GetColor(kOrange+1);
 TColor *kDAMPE_proton = gROOT->GetColor(kRed);
-TColor *kAMS02_electron = gROOT->GetColor(kBlue-10);
+TColor *kAMS02_electron = gROOT->GetColor(kBlue-2);
+TColor *kAMS02_positron = gROOT->GetColor(kBlue-10);
+TColor *kAMS02_pbar = gROOT->GetColor(kRed-9);
+TColor *kAMS02_B = gROOT->GetColor(kMagenta-2);
+TColor *kAMS02_C = gROOT->GetColor(kGray+2);
+TColor *kAMS02_O = gROOT->GetColor(kOrange-6);
 TColor *kCALET_electron = gROOT->GetColor(kCyan-6);
 TColor *kDAMPE_electron = gROOT->GetColor(kBlue);
 TColor *kFERMILAT_electron = gROOT->GetColor(kAzure-4);
@@ -42,17 +52,23 @@ TColor *kKASCADEgrande_allparticle = gROOT->GetColor(kGray+2);
 TColor *kATIC2_proton = gROOT->GetColor(kMagenta+2);
 TColor *kAMS02_helium = gROOT->GetColor(kGreen+2);
 
-int ams02_marker_style = 21;
-int dampe_marker_style = 20;
+int ams02_marker_style = 107;
+int dampe_marker_style = 108;
 int calet_marker_style = 33;
 int fermi_marker_style = 45;
 int auger_marker_style = 108;
-int kascadegrande_marker_style = 107;
+int kascadegrande_marker_style = 20;
 int atic2_marker_style = 110;
 
 const char* ams02_proton_legendentry = "Protons (AMS02)";
 const char* calet_proton_legendentry = "Protons (CALET)";
 const char* dampe_proton_legendentry = "Protons (DAMPE)";
+const char* ams02_electron_legendentry = "e^{-} (AMS02)";
+const char* ams02_positron_legendentry = "e^{+} (AMS02)";
+const char* ams02_pbar_legendentry = "#bar{p} (AMS02)";
+const char* ams02_B_legendentry = "B (AMS02)";
+const char* ams02_C_legendentry = "C (AMS02)";
+const char* ams02_O_legendentry = "O (AMS02)";
 const char* ams02_allele_legendentry = "e^{-}+e^{+} (AMS02)";
 const char* calet_allele_legendentry = "e^{-}+e^{+} (CALET)";
 const char* fermilat_allele_legendentry = "e^{-}+e^{+} (FERMI-LAT)";
@@ -64,6 +80,11 @@ const char* ams02_helium_legendentry = "Helium (AMS02)";
 
 const char* ams02_proton_path = "data/ssdc_AMS02_p_E3.root";
 const char* ams02_electron_path = "data/ssdc_AMS02_e_E3.root";
+const char* ams02_positron_path = "data/ssdc_AMS02_positron_E3.root";
+const char* ams02_pbar_path = "data/in2p3_crdb_AMS02_pbar_E3.root";
+const char* ams02_B_path = "data/ssdc_AMS02_B_E3.root";
+const char* ams02_C_path = "data/ssdc_AMS02_C_E3.root";
+const char* ams02_O_path = "data/ssdc_AMS02_O_E3.root";
 const char* calet_proton_path = "data/ssdc_CALET_p_E3.root";
 const char* dampe_proton_path = "data/ssdc_DAMPE_p_E3.root";
 const char* dampe_electron_path = "data/ssdc_DAMPE_e_E3.root";
@@ -76,9 +97,10 @@ const char* ams02_helium_path = "data/ssdc_AMS02_He_E3.root";
 
 std::vector<std::string> auger_filter = {"gr_exp1_errtot", "gr_exp1_upper_limit"};
 std::vector<std::string> kascadegrande_filter = {"gr_exp1_errtot"};
+std::vector<std::string> ams02_pbar_filter = {"gr_exp1_errtot"};
 
 const double emin = 1;
-const double emax = 1e+12;
+const double emax = 1e+5;
 const double phi_min = 1;
 const double phi_max = 1e+7;
 
@@ -116,7 +138,12 @@ std::shared_ptr<TMultiGraph> buildflux()
     if (CALET_proton) add_to_mg(calet_proton_path, kCALET_proton, calet_marker_style, 1.3, calet_proton_legendentry, mgE3);
     if (ATIC2_proton) add_to_mg(atic2_proton_path, kATIC2_proton, atic2_marker_style, 1.3, atic2_proton_legendentry, mgE3);
     if (DAMPE_proton) add_to_mg(dampe_proton_path, kDAMPE_proton, dampe_marker_style, 1, dampe_proton_legendentry, mgE3);
-    if (AMS02_electron) add_to_mg(ams02_electron_path, kAMS02_electron, ams02_marker_style, 1, ams02_allele_legendentry, mgE3);
+    if (AMS02_electron) add_to_mg(ams02_electron_path, kAMS02_electron, ams02_marker_style, 1, ams02_electron_legendentry, mgE3);
+    if (AMS02_positron) add_to_mg(ams02_positron_path, kAMS02_positron, ams02_marker_style, 1, ams02_positron_legendentry, mgE3);
+    if (AMS02_pbar) add_to_mg(ams02_pbar_path, kAMS02_pbar, ams02_marker_style, 1, ams02_pbar_legendentry, mgE3, ams02_pbar_filter);
+    if (AMS02_B) add_to_mg(ams02_B_path, kAMS02_B, ams02_marker_style, 1, ams02_B_legendentry, mgE3);
+    if (AMS02_C) add_to_mg(ams02_C_path, kAMS02_C, ams02_marker_style, 1, ams02_C_legendentry, mgE3);
+    if (AMS02_O) add_to_mg(ams02_O_path, kAMS02_O, ams02_marker_style, 1, ams02_O_legendentry, mgE3);
     if (CALET_electron) add_to_mg(calet_electron_path, kCALET_electron, calet_marker_style, 1.3, calet_allele_legendentry, mgE3);
     if (FERMILAT_electron) add_to_mg(fermilat_electron_path, kFERMILAT_electron, fermi_marker_style, 1, fermilat_allele_legendentry, mgE3);
     if (DAMPE_electron) add_to_mg(dampe_electron_path, kDAMPE_electron, dampe_marker_style, 1, dampe_allele_legendentry, mgE3);
@@ -172,6 +199,14 @@ void add_to_mg(
             tmp_obj->SetMarkerStyle(marker_style);
             tmp_obj->SetMarkerSize(marker_size);
             tmp_obj->SetTitle(gr_title);
+            
+            // Set error to zero along X axis
+            for (int i = 0; i < tmp_obj->GetN(); ++i)
+            {
+                tmp_obj->SetPointEXlow(i, 0);
+                tmp_obj->SetPointEXhigh(i, 0);
+            }
+
             mgE3->Add(tmp_obj);
         }
     }
