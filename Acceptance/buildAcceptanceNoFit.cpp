21995f29fa111b9f945163c075c0560b34a46d2b
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-03-08 14:00:36 +0100

    BGO fiducial volume and HET are two different acceptances

--- Acceptance/buildAcceptanceNoFit.cpp
+++ Acceptance/buildAcceptanceNoFit.cpp
@@ -35,12 +35,22 @@ void buildAcceptance(
     TH1D* h_geometric               = static_cast<TH1D*>(input_file->Get("h_geometric"));
     TH1D* h_geometric_trigger       = static_cast<TH1D*>(input_file->Get("h_geometric_trigger"));
     TH1D* h_bgo_fiducial            = static_cast<TH1D*>(input_file->Get("h_bgo_fiducial"));
+    TH1D* h_bgo_fiducial_het        = static_cast<TH1D*>(input_file->Get("h_bgo_fiducial_het"));
+    TH1D* h_nBarLayer13             = static_cast<TH1D*>(input_file->Get("h_nBarLayer13"));
+    TH1D* h_maxrms                  = static_cast<TH1D*>(input_file->Get("h_maxrms"));
+    TH1D* h_trackselection          = static_cast<TH1D*>(input_file->Get("h_trackselection"));
+    TH1D* h_psdstkmatch             = static_cast<TH1D*>(input_file->Get("h_psdstkmatch"));
     TH1D* h_all_cut                 = static_cast<TH1D*>(input_file->Get("h_all_cut"));
 
     h_gen                           ->SetDirectory(0);
     h_geometric                     ->SetDirectory(0);
     h_geometric_trigger             ->SetDirectory(0);
     h_bgo_fiducial                  ->SetDirectory(0);
+    h_bgo_fiducial_het              ->SetDirectory(0);
+    h_nBarLayer13                   ->SetDirectory(0);
+    h_maxrms                        ->SetDirectory(0);
+    h_trackselection                ->SetDirectory(0);
+    h_psdstkmatch                   ->SetDirectory(0);
     h_all_cut                       ->SetDirectory(0);
 
     input_file->Close();
@@ -48,11 +58,21 @@ void buildAcceptance(
     TH1D* h_geometric_factor        = static_cast<TH1D*>(h_geometric->Clone("h_geometric_factor"));
     TH1D* h_acc_geometric_trigger   = static_cast<TH1D*>(h_geometric_trigger->Clone("h_acc_geometric_trigger"));
     TH1D* h_acc_bgo_fiducial        = static_cast<TH1D*>(h_bgo_fiducial->Clone("h_acc_bgo_fiducial"));
+    TH1D* h_acc_bgo_fiducial_het    = static_cast<TH1D*>(h_bgo_fiducial_het->Clone("h_acc_bgo_fiducial_het"));
+    TH1D* h_acc_nBarLayer13         = static_cast<TH1D*>(h_nBarLayer13->Clone("h_acc_nBarLayer13"));
+    TH1D* h_acc_maxrms              = static_cast<TH1D*>(h_maxrms->Clone("h_acc_maxrms"));
+    TH1D* h_acc_trackselection      = static_cast<TH1D*>(h_trackselection->Clone("h_acc_trackselection"));
+    TH1D* h_acc_psdstkmatch         = static_cast<TH1D*>(h_psdstkmatch->Clone("h_acc_psdstkmatch"));
     TH1D* h_acc_all_cut             = static_cast<TH1D*>(h_all_cut->Clone("h_acc_all_cut"));
 
     h_geometric_factor              ->Divide(h_gen);
     h_acc_geometric_trigger         ->Divide(h_gen);
     h_acc_bgo_fiducial              ->Divide(h_gen);
+    h_acc_bgo_fiducial_het          ->Divide(h_gen);
+    h_acc_nBarLayer13               ->Divide(h_gen);
+    h_acc_maxrms                    ->Divide(h_gen);
+    h_acc_trackselection            ->Divide(h_gen);
+    h_acc_psdstkmatch               ->Divide(h_gen);
     h_acc_all_cut                   ->Divide(h_gen);
 
     double genSurface               = 4 * TMath::Pi() * pow(generation_vertex_radius, 2) / 2;
@@ -61,21 +81,41 @@ void buildAcceptance(
     h_geometric_factor              ->Scale(scaleFactor);
     h_acc_geometric_trigger         ->Scale(scaleFactor);
     h_acc_bgo_fiducial              ->Scale(scaleFactor);
+    h_acc_bgo_fiducial_het          ->Scale(scaleFactor);
+    h_acc_nBarLayer13               ->Scale(scaleFactor);
+    h_acc_maxrms                    ->Scale(scaleFactor);
+    h_acc_trackselection            ->Scale(scaleFactor);
+    h_acc_psdstkmatch               ->Scale(scaleFactor);
     h_acc_all_cut                   ->Scale(scaleFactor);
 
     h_geometric_factor              ->GetYaxis()->SetTitle("geometric factor [m^{2} sr]");
     h_acc_geometric_trigger         ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
     h_acc_bgo_fiducial              ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    h_acc_bgo_fiducial_het          ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    h_acc_nBarLayer13               ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    h_acc_maxrms                    ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    h_acc_trackselection            ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    h_acc_psdstkmatch               ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
     h_acc_all_cut                   ->GetYaxis()->SetTitle("acceptance [m^{2} sr]");
 
     std::vector<double> geometric_factor                (h_geometric_factor->GetNbinsX(), 0);
     std::vector<double> acc_geometric_trigger           (h_geometric_factor->GetNbinsX(), 0);
     std::vector<double> acc_bgo_fiducial                (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_bgo_fiducial_het            (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_nBarLayer13                 (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_maxrms                      (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_trackselection              (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_psdstkmatch                 (h_geometric_factor->GetNbinsX(), 0);
     std::vector<double> acc_all_cut                     (h_geometric_factor->GetNbinsX(), 0);
 
     std::vector<double> geometric_factor_err            (h_geometric_factor->GetNbinsX(), 0);
     std::vector<double> acc_geometric_trigger_err       (h_geometric_factor->GetNbinsX(), 0);
     std::vector<double> acc_bgo_fiducial_err            (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_bgo_fiducial_het_err        (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_nBarLayer13_err             (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_maxrms_err                  (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_trackselection_err          (h_geometric_factor->GetNbinsX(), 0);
+    std::vector<double> acc_psdstkmatch_err             (h_geometric_factor->GetNbinsX(), 0);
     std::vector<double> acc_all_cut_err                 (h_geometric_factor->GetNbinsX(), 0);
 
     std::vector<double> energy                          (h_geometric_factor->GetNbinsX(), 0);
@@ -83,41 +123,76 @@ void buildAcceptance(
 
     for (unsigned int idx=0; idx<energy.size(); ++idx)
     {
-        energy[idx] =                       wtsydp(energy_binning[idx], energy_binning[idx+1], -2.7);
-        geometric_factor[idx] =             h_geometric_factor->GetBinContent(idx+1);
-        geometric_factor_err[idx] =         h_geometric_factor->GetBinError(idx+1);
-        acc_geometric_trigger[idx] =        h_acc_geometric_trigger->GetBinContent(idx+1);
-        acc_geometric_trigger_err[idx] =    h_acc_geometric_trigger->GetBinError(idx+1);
-        acc_bgo_fiducial[idx] =             h_acc_bgo_fiducial->GetBinContent(idx+1);
-        acc_bgo_fiducial_err[idx] =         h_acc_bgo_fiducial->GetBinError(idx+1);
-        acc_all_cut[idx] =                  h_acc_all_cut->GetBinContent(idx+1);
-        acc_all_cut_err[idx] =              h_acc_all_cut->GetBinError(idx+1);
+        energy[idx]                         = wtsydp(energy_binning[idx], energy_binning[idx+1], -2.7);
+        geometric_factor[idx]               = h_geometric_factor->GetBinContent(idx+1);
+        geometric_factor_err[idx]           = h_geometric_factor->GetBinError(idx+1);
+        acc_geometric_trigger[idx]          = h_acc_geometric_trigger->GetBinContent(idx+1);
+        acc_geometric_trigger_err[idx]      = h_acc_geometric_trigger->GetBinError(idx+1);
+        acc_bgo_fiducial[idx]               = h_acc_bgo_fiducial->GetBinContent(idx+1);
+        acc_bgo_fiducial_err[idx]           = h_acc_bgo_fiducial->GetBinError(idx+1);
+        acc_bgo_fiducial_het[idx]           = h_acc_bgo_fiducial_het->GetBinContent(idx+1);
+        acc_bgo_fiducial_het_err[idx]       = h_acc_bgo_fiducial_het->GetBinError(idx+1);
+        acc_nBarLayer13[idx]                = h_acc_nBarLayer13->GetBinContent(idx+1);
+        acc_nBarLayer13_err[idx]            = h_acc_nBarLayer13->GetBinError(idx+1);
+        acc_maxrms[idx]                     = h_acc_maxrms->GetBinContent(idx+1);
+        acc_maxrms_err[idx]                 = h_acc_maxrms->GetBinError(idx+1);
+        acc_trackselection[idx]             = h_acc_trackselection->GetBinContent(idx+1);
+        acc_trackselection_err[idx]         = h_acc_trackselection->GetBinError(idx+1);
+        acc_psdstkmatch[idx]                = h_acc_psdstkmatch->GetBinContent(idx+1);
+        acc_psdstkmatch_err[idx]            = h_acc_psdstkmatch->GetBinError(idx+1);
+        acc_all_cut[idx]                    = h_acc_all_cut->GetBinContent(idx+1);
+        acc_all_cut_err[idx]                = h_acc_all_cut->GetBinError(idx+1);
     }
     
     TGraphErrors gr_geometric_factor            ((int)geometric_factor.size(), &energy[0], &geometric_factor[0], &energy_err[0], &geometric_factor_err[0]);
     TGraphErrors gr_acc_geometric_trigger       ((int)geometric_factor.size(), &energy[0], &acc_geometric_trigger[0], &energy_err[0], &acc_geometric_trigger_err[0]);
     TGraphErrors gr_acc_bgo_fiducial            ((int)geometric_factor.size(), &energy[0], &acc_bgo_fiducial[0], &energy_err[0], &acc_bgo_fiducial_err[0]);
+    TGraphErrors gr_acc_bgo_fiducial_het        ((int)geometric_factor.size(), &energy[0], &acc_bgo_fiducial_het[0], &energy_err[0], &acc_bgo_fiducial_het_err[0]);
+    TGraphErrors gr_acc_nBarLayer13             ((int)geometric_factor.size(), &energy[0], &acc_nBarLayer13[0], &energy_err[0], &acc_nBarLayer13_err[0]);
+    TGraphErrors gr_acc_maxrms                  ((int)geometric_factor.size(), &energy[0], &acc_maxrms[0], &energy_err[0], &acc_maxrms_err[0]);
+    TGraphErrors gr_acc_trackselection          ((int)geometric_factor.size(), &energy[0], &acc_trackselection[0], &energy_err[0], &acc_trackselection_err[0]);
+    TGraphErrors gr_acc_psdstkmatch             ((int)geometric_factor.size(), &energy[0], &acc_psdstkmatch[0], &energy_err[0], &acc_psdstkmatch_err[0]);
     TGraphErrors gr_acc_all_cut                 ((int)geometric_factor.size(), &energy[0], &acc_all_cut[0], &energy_err[0], &acc_all_cut_err[0]);
 
-    gr_geometric_factor.SetName("gr_geometric_factor");
-    gr_acc_geometric_trigger.SetName("gr_acc_geometric_trigger");
-    gr_acc_bgo_fiducial.SetName("gr_acc_bgo_fiducial");
-    gr_acc_all_cut.SetName("gr_acc_all_cut");
+    gr_geometric_factor                     .SetName("gr_geometric_factor");
+    gr_acc_geometric_trigger                .SetName("gr_acc_geometric_trigger");
+    gr_acc_bgo_fiducial                     .SetName("gr_acc_bgo_fiducial");
+    gr_acc_bgo_fiducial_het                 .SetName("gr_acc_bgo_fiducial_het");
+    gr_acc_nBarLayer13                      .SetName("gr_acc_nBarLayer13");
+    gr_acc_maxrms                           .SetName("gr_acc_maxrms");
+    gr_acc_trackselection                   .SetName("gr_acc_trackselection");
+    gr_acc_psdstkmatch                      .SetName("gr_acc_psdstkmatch");
+    gr_acc_all_cut                          .SetName("gr_acc_all_cut");
 
-    gr_geometric_factor.SetTitle("Geometric Factor");
-    gr_acc_geometric_trigger.SetTitle("Acceptance - geometric + trigger");
-    gr_acc_bgo_fiducial.SetTitle("Acceptance - BGO fiducial");
-    gr_acc_all_cut.SetTitle("Acceptance - all cuts");
+    gr_geometric_factor                     .SetTitle("Geometric Factor");
+    gr_acc_geometric_trigger                .SetTitle("Acceptance - geometric + trigger");
+    gr_acc_bgo_fiducial                     .SetTitle("Acceptance - BGO fiducial");
+    gr_acc_bgo_fiducial_het                 .SetTitle("Acceptance - BGO fiducial + HET");
+    gr_acc_nBarLayer13                      .SetTitle("Acceptance - nBarLayer13");
+    gr_acc_maxrms                           .SetTitle("Acceptance - maxrms");
+    gr_acc_trackselection                   .SetTitle("Acceptance - track selection");
+    gr_acc_psdstkmatch                      .SetTitle("Acceptance - PSD-STK match");
+    gr_acc_all_cut                          .SetTitle("Acceptance - all cuts");
 
-    gr_geometric_factor.GetXaxis()->SetTitle("Energy [GeV]");
-    gr_acc_geometric_trigger.GetXaxis()->SetTitle("Energy [GeV]");
-    gr_acc_bgo_fiducial.GetXaxis()->SetTitle("Energy [GeV]");
-    gr_acc_all_cut.GetXaxis()->SetTitle("Energy [GeV]");
+    gr_geometric_factor                     .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_geometric_trigger                .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_bgo_fiducial                     .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_bgo_fiducial_het                 .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_nBarLayer13                      .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_maxrms                           .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_trackselection                   .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_psdstkmatch                      .GetXaxis()->SetTitle("Energy [GeV]");
+    gr_acc_all_cut                          .GetXaxis()->SetTitle("Energy [GeV]");
 
-    gr_geometric_factor.GetYaxis()->SetTitle("geometric factor [m^{2} sr]");
-    gr_acc_geometric_trigger.GetYaxis()->SetTitle("acceptance [m^{2} sr]");
-    gr_acc_bgo_fiducial.GetYaxis()->SetTitle("acceptance [m^{2} sr]");
-    gr_acc_all_cut.GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_geometric_factor                     .GetYaxis()->SetTitle("geometric factor [m^{2} sr]");
+    gr_acc_geometric_trigger                .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_acc_bgo_fiducial                     .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_acc_bgo_fiducial_het                 .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_acc_nBarLayer13                      .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_acc_maxrms                           .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_acc_trackselection                   .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_acc_psdstkmatch                      .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
+    gr_acc_all_cut                          .GetYaxis()->SetTitle("acceptance [m^{2} sr]");
 
     TFile* outfile = TFile::Open(output_file, "RECREATE");
     if (outfile->IsZombie())
@@ -126,15 +201,25 @@ void buildAcceptance(
         exit(100);
     }
 
-    h_geometric_factor->Write();
-    h_acc_geometric_trigger->Write();
-    h_acc_bgo_fiducial->Write();
-    h_acc_all_cut->Write();
+    h_geometric_factor          ->Write();
+    h_acc_geometric_trigger     ->Write();
+    h_acc_bgo_fiducial          ->Write();
+    h_acc_bgo_fiducial_het      ->Write();
+    h_acc_nBarLayer13           ->Write();
+    h_acc_maxrms                ->Write();
+    h_acc_trackselection        ->Write();
+    h_acc_psdstkmatch           ->Write();
+    h_acc_all_cut               ->Write();
 
-    gr_geometric_factor.Write();
-    gr_acc_geometric_trigger.Write();
-    gr_acc_bgo_fiducial.Write();
-    gr_acc_all_cut.Write();
+    gr_geometric_factor         .Write();
+    gr_acc_geometric_trigger    .Write();
+    gr_acc_bgo_fiducial         .Write();
+    gr_acc_bgo_fiducial_het     .Write();
+    gr_acc_nBarLayer13          .Write();
+    gr_acc_maxrms               .Write();
+    gr_acc_trackselection       .Write();
+    gr_acc_psdstkmatch          .Write();
+    gr_acc_all_cut              .Write();
 
     outfile->Close();
 
