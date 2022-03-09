962699a40b522dc6bfd26faf12ec9f0121094ac7
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-03-09 17:16:20 +0100

    HET trigger has been added by default in the efficiency

--- Collector/source/Efficiency/efficiency.cpp
+++ Collector/source/Efficiency/efficiency.cpp
@@ -30,97 +30,103 @@ void efficiency::Pipeline(
                                     }
 
         // MaxRms efficiency
-        if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
-            if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
-                if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
-                    if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
-                        if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
-                            if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
-                                if (psd_charge_cut(psdVault.getPsdClusterMaxE(), psdVault.getPsdClusterIdxMaxE(), psdVault.getHitZ(), psdVault.getGlobalBarID(), cuts))
-                                {
-                                    output.maxrms_efficiency_preselection = true;
-                                    if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
-                                        output.maxrms_efficiency_preselection_accepted = true;
-                                    else
-                                        output.maxrms_efficiency_preselection_notaccepted = true;
-                                }
+        if (output.evt_triggered)
+            if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
+                if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
+                    if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
+                        if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
+                            if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
+                                if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
+                                    if (psd_charge_cut(psdVault.getPsdClusterMaxE(), psdVault.getPsdClusterIdxMaxE(), psdVault.getHitZ(), psdVault.getGlobalBarID(), cuts))
+                                    {
+                                        output.maxrms_efficiency_preselection = true;
+                                        if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
+                                            output.maxrms_efficiency_preselection_accepted = true;
+                                        else
+                                            output.maxrms_efficiency_preselection_notaccepted = true;
+                                    }
 
         // nbarlayer13 efficiency
-        if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
-            if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
-                if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
-                    if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
+        if (output.evt_triggered)
+            if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
+                if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
+                    if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
+                        if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
+                            if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
+                                if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
+                                    if (psd_charge_cut(psdVault.getPsdClusterMaxE(), psdVault.getPsdClusterIdxMaxE(), psdVault.getHitZ(), psdVault.getGlobalBarID(), cuts))
+                                    {
+                                        output.nbarlayer13_efficiency_preselection = true;
+                                        if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
+                                            output.nbarlayer13_efficiency_preselection_accepted = true;
+                                        else
+                                            output.nbarlayer13_efficiency_preselection_notaccepted = true;
+                                    }
+
+        // MaxRms && nbarlayer13 efficiency
+        if (output.evt_triggered)
+            if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
+                if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
+                    if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
                         if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
                             if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
                                 if (psd_charge_cut(psdVault.getPsdClusterMaxE(), psdVault.getPsdClusterIdxMaxE(), psdVault.getHitZ(), psdVault.getGlobalBarID(), cuts))
                                 {
-                                    output.nbarlayer13_efficiency_preselection = true;
-                                    if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
-                                        output.nbarlayer13_efficiency_preselection_accepted = true;
+                                    output.maxrms_and_nbarlayer13_efficiency_preselection = true;
+                                    if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts) && nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
+                                        output.maxrms_and_nbarlayer13_efficiency_preselection_accepted = true;
                                     else
-                                        output.nbarlayer13_efficiency_preselection_notaccepted = true;
+                                        output.maxrms_and_nbarlayer13_efficiency_preselection_notaccepted = true;
                                 }
 
-        // MaxRms && nbarlayer13 efficiency
-        if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
-            if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
-                if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
-                    if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
-                        if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
-                            if (psd_charge_cut(psdVault.getPsdClusterMaxE(), psdVault.getPsdClusterIdxMaxE(), psdVault.getHitZ(), psdVault.getGlobalBarID(), cuts))
-                            {
-                                output.maxrms_and_nbarlayer13_efficiency_preselection = true;
-                                if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts) && nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
-                                    output.maxrms_and_nbarlayer13_efficiency_preselection_accepted = true;
-                                else
-                                    output.maxrms_and_nbarlayer13_efficiency_preselection_notaccepted = true;
-                            }
-
         // track selection efficiency
-        if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
-            if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
-                if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
-                    if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
-                        if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
-                        {
-                            output.track_efficiency_preselection = true;
-                            if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
-                                output.track_efficiency_preselection_accepted = true;
-                            else
-                                output.track_efficiency_preselection_notaccepted = true;
-                        }
+        if (output.evt_triggered)
+            if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
+                if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
+                    if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
+                        if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
+                            if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
+                            {
+                                output.track_efficiency_preselection = true;
+                                if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
+                                    output.track_efficiency_preselection_accepted = true;
+                                else
+                                    output.track_efficiency_preselection_notaccepted = true;
+                            }
                                         
         // psd_stk_match efficiency
-        if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
-            if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
-                if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
-                    if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
-                        if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
-                            if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
-                            {
-                                output.psdstkmatch_efficiency_preselection = true;
-                                if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
-                                    output.psdstkmatch_efficiency_preselection_accepted = true;
-                                else
-                                    output.psdstkmatch_efficiency_preselection_notaccepted = true;
-                            }
+        if (output.evt_triggered)
+            if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
+                if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
+                    if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
+                        if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
+                            if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
+                                if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
+                                {
+                                    output.psdstkmatch_efficiency_preselection = true;
+                                    if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
+                                        output.psdstkmatch_efficiency_preselection_accepted = true;
+                                    else
+                                        output.psdstkmatch_efficiency_preselection_notaccepted = true;
+                                }
 
         // psd_charge efficiency
-        if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
-            if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
-                if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
-                    if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
-                        if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
-                            if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
-                                if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
-                                    if (stk_charge_cut(stkclusters, 11))
-                                    {
-                                        output.psdcharge_efficiency_preselection = true;
-                                        if (psd_charge_cut(psdVault.getPsdClusterMaxE(), psdVault.getPsdClusterIdxMaxE(), psdVault.getHitZ(), psdVault.getGlobalBarID(), cuts))
-                                            output.psdcharge_efficiency_preselection_accepted = true;
-                                        else
-                                            output.psdcharge_efficiency_preselection_notaccepted = true;
-                                    }
+        if (output.evt_triggered)
+            if (maxElayer_cut(bgoVault.GetLayerEnergies(), cuts, bgoTotalE))
+                if (maxBarLayer_cut(bgoVault.GetLayerBarNumber(), bgoVault.GetiMaxLayer(), bgoVault.GetIdxBarMaxLayer()))
+                    if (BGOTrackContainment_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts))
+                        if (nBarLayer13_cut(bgohits, bgoVault.GetSingleLayerBarNumber(13), bgoTotalE))
+                            if (maxRms_cut(bgoVault.GetELayer(), bgoVault.GetRmsLayer(), bgoTotalE, cuts))
+                                if (track_selection_cut(bgorec, bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), bgohits, stkclusters, stktracks, cuts))
+                                    if (psd_stk_match_cut(bgoVault.GetBGOslope(), bgoVault.GetBGOintercept(), cuts, psdVault.getPsdClusterIdxBegin(), psdVault.getPsdClusterZ(), psdVault.getPsdClusterMaxECoo()))
+                                        if (stk_charge_cut(stkclusters, 11))
+                                        {
+                                            output.psdcharge_efficiency_preselection = true;
+                                            if (psd_charge_cut(psdVault.getPsdClusterMaxE(), psdVault.getPsdClusterIdxMaxE(), psdVault.getHitZ(), psdVault.getGlobalBarID(), cuts))
+                                                output.psdcharge_efficiency_preselection_accepted = true;
+                                            else
+                                                output.psdcharge_efficiency_preselection_notaccepted = true;
+                                        }
     }
 
 const eff_output efficiency::GetEfficiencyOutput()
