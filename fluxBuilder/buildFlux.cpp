0edcb87129a695f68d5eb439d42ae40533305294
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-05-05 14:15:10 +0200

    Minor modifications

--- fluxBuilder/buildFlux.cpp
+++ fluxBuilder/buildFlux.cpp
@@ -1,8 +1,76 @@
+#include <vector>
+#include <memory>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include <iostream>
 
 #include "TKey.h"
 #include "TH1D.h"
 #include "TFile.h"
+#include "TGraphErrors.h"
+
+struct energy_config {
+    std::size_t n_bins;
+    double min_event_energy {-999};
+    double max_event_energy {-999};
+    std::vector<float> energy_binning;
+    
+    void createLogBinning() {
+        energy_binning = std::vector<float>(n_bins + 1, 0);
+        double log_interval {(log10(max_event_energy)-log10(min_event_energy))/n_bins};
+        for (unsigned int bIdx = 0; bIdx <= n_bins; ++bIdx)
+            energy_binning[bIdx] = pow(10, log10(min_event_energy) + bIdx * log_interval);
+    }
+};
+
+inline std::string parse_config_file(const char* config_file) {
+	std::ifstream input_file(config_file);
+	if (!input_file.is_open()) {
+		std::cerr << "\nInput config file not found [" << config_file << "]\n\n";
+		exit(100);
+	}
+	std::string input_string(
+		(std::istreambuf_iterator<char>(input_file)),
+		(std::istreambuf_iterator<char>()));
+	input_file.close();
+	return input_string;
+}
+
+inline energy_config get_config_info(const std::string parsed_config) {
+	energy_config config_pars;
+    std::string tmp_str;
+	std::istringstream input_stream(parsed_config);
+	std::string::size_type sz;
+	while (input_stream >> tmp_str)
+	{
+		if (!strcmp(tmp_str.c_str(), "n_energy_bins"))
+			input_stream >> config_pars.n_bins;
+		if (!strcmp(tmp_str.c_str(), "min_event_energy")) {
+			input_stream >> tmp_str;
+			config_pars.min_event_energy = stod(tmp_str, &sz);
+		}
+		if (!strcmp(tmp_str.c_str(), "max_event_energy")) {
+			input_stream >> tmp_str;
+			config_pars.max_event_energy = stod(tmp_str, &sz);
+		}
+	}
+    config_pars.createLogBinning();
+    return config_pars;
+}
+
+inline std::vector<float> parse_energy_config(const char* config_file) {
+    return get_config_info(parse_config_file(config_file)).energy_binning;
+}
+
+inline double wtsydp(const double minene, const double maxene, const double index)
+{
+    auto dene = maxene - minene;
+    if (index != -1)
+        return pow(fabs((pow(maxene, index + 1) - pow(minene, index + 1)) / ((index + 1) * dene)), 1. / index);
+    else
+        return dene / log(maxene / minene);
+}
 
 inline std::shared_ptr<TH1D> extractHistoFromFile(const char* file, const bool verbose) {
     TFile* infile {TFile::Open(file, "READ")};
@@ -32,12 +100,17 @@ void buildFlux(
     const char* e_counts_input_file,
     const char* e_acc_input_file,
     const char* signal_efficiency,
+    const char* energy_config_file,
     const double exposure_time = 140143948.911,
     const char* output_file = "flux.root",
     const bool verbose = true) {
 
         if (verbose)
             std::cout << "\n\nReading input files... " << std::endl;
+
+        // Extract energy binning from config file
+        auto energy_binning = parse_energy_config(energy_config_file);
+        auto energy_nbins = (int)energy_binning.size() - 1;
 
         // Extract info from input files
         auto h_e_counts = extractHistoFromFile(e_counts_input_file, verbose);
@@ -69,6 +142,37 @@ void buildFlux(
                 h_e_counts_E3->SetBinError(bIdx, h_e_counts_E3->GetBinError(bIdx)*pow(h_e_counts_E3->GetXaxis()->GetBinCenter(bIdx), 3));
             }
         }
+        
+        // Build TGraphs
+        std::vector<double> energy                          (h_e_counts->GetNbinsX(), 0);
+        std::vector<double> energy_err                      (h_e_counts->GetNbinsX(), 0);
+
+        std::vector<double> flux                            (h_e_counts->GetNbinsX(), 0);
+        std::vector<double> flux_err                        (h_e_counts->GetNbinsX(), 0);
+
+        std::vector<double> flux_E3                         (h_e_counts->GetNbinsX(), 0);
+        std::vector<double> flux_E3_err                     (h_e_counts->GetNbinsX(), 0);
+
+        for (unsigned int idx=0; idx<energy.size(); ++idx) {
+            energy[idx]         = wtsydp(energy_binning[idx], energy_binning[idx+1], -3);
+            flux[idx]           = h_e_counts->GetBinContent(idx+1);
+            flux_err[idx]       = h_e_counts->GetBinError(idx+1);
+            flux_E3[idx]        = h_e_counts_E3->GetBinContent(idx+1);
+            flux_E3_err[idx]    = h_e_counts_E3->GetBinError(idx+1);
+        }
+
+        TGraphErrors gr_flux(energy.size(), &energy[0], &flux[0], &energy_err[0], &flux_err[0]);
+        TGraphErrors gr_flux_E3(energy.size(), &energy[0], &flux_E3[0], &energy_err[0], &flux_E3_err[0]);
+
+        gr_flux.SetName("gr_flux");
+        gr_flux.SetTitle("Flux");
+        gr_flux.GetXaxis()->SetTitle("Energy [GeV]");
+        gr_flux.GetYaxis()->SetTitle("Flux [s^{-1}E^{-1}st^{-1}]");
+
+        gr_flux_E3.SetName("gr_flux_E3");
+        gr_flux_E3.SetTitle("Flux");
+        gr_flux_E3.GetXaxis()->SetTitle("Energy [GeV]");
+        gr_flux_E3.GetYaxis()->SetTitle("Flux E^{3}*[s^{-1}E^{-1}st^{-1}]");
 
         // Write output file
         TFile *outfile {TFile::Open(output_file, "RECREATE")};
@@ -86,6 +190,9 @@ void buildFlux(
 
         h_e_counts->Write();
         h_e_counts_E3->Write();
+
+        gr_flux.Write();
+        gr_flux_E3.Write();
 
         outfile->Close();
 
