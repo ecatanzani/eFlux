51a73a07f73b16473fba318b91f5a5f0df5dccc8
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-05-10 14:22:51 +0200

    Print binning based on input energy config file

---
+++ assets/printBinning.cpp
@@ -0,0 +1,78 @@
+#include <string>
+#include <vector>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+
+struct energy_config {
+    std::size_t n_bins;
+    double min_event_energy {-999};
+    double max_event_energy {-999};
+    std::vector<float> energy_binning;
+    
+    void createLogBinning() {
+        energy_binning = std::vector<float>(n_bins + 1, 0);
+        double log_interval {(log10(max_event_energy)-log10(min_event_energy))/n_bins};
+        for (unsigned int bIdx = 0; bIdx <= n_bins; ++bIdx)
+            energy_binning[bIdx] = pow(10, log10(min_event_energy) + bIdx * log_interval);
+    }
+};
+
+inline std::string parse_config_file(const char* config_file) {
+	std::ifstream input_file(config_file);
+	if (!input_file.is_open()) {
+		std::cerr << "\nInput config file not found [" << config_file << "]\n\n";
+		exit(100);
+	}
+	std::string input_string(
+		(std::istreambuf_iterator<char>(input_file)),
+		(std::istreambuf_iterator<char>()));
+	input_file.close();
+	return input_string;
+}
+
+inline energy_config get_config_info(const std::string parsed_config) {
+	energy_config config_pars;
+    std::string tmp_str;
+	std::istringstream input_stream(parsed_config);
+	std::string::size_type sz;
+	while (input_stream >> tmp_str)
+	{
+		if (!strcmp(tmp_str.c_str(), "n_energy_bins"))
+			input_stream >> config_pars.n_bins;
+		if (!strcmp(tmp_str.c_str(), "min_event_energy")) {
+			input_stream >> tmp_str;
+			config_pars.min_event_energy = stod(tmp_str, &sz);
+		}
+		if (!strcmp(tmp_str.c_str(), "max_event_energy")) {
+			input_stream >> tmp_str;
+			config_pars.max_event_energy = stod(tmp_str, &sz);
+		}
+	}
+    config_pars.createLogBinning();
+    return config_pars;
+}
+
+inline std::vector<float> parse_energy_config(const char* config_file) {
+    return get_config_info(parse_config_file(config_file)).energy_binning;
+}
+
+inline double wtsydp(const double minene, const double maxene, const double index)
+{
+    auto dene = maxene - minene;
+    if (index != -1)
+        return pow(fabs((pow(maxene, index + 1) - pow(minene, index + 1)) / ((index + 1) * dene)), 1. / index);
+    else
+        return dene / log(maxene / minene);
+}
+
+void printBinning(const char* energy_config_file) {
+    auto energy_binning = parse_energy_config(energy_config_file);
+
+    for (size_t idx = 0; idx < energy_binning.size()-1; ++idx)
+        std::cout << "\nBin " << idx + 1 << " \t " << energy_binning[idx] << " \t " << energy_binning[idx+1] << " \t " << wtsydp(energy_binning[idx], energy_binning[idx+1], -3);
+
+    std::cout << "\n\nComplete!\n\n";
+}
