2f105bd8e3230da21d2f3c995ce3d7ce9483c8a3
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-05-12 14:58:11 +0200

    Updated with error along the flux best point

--- fluxBuilder/classifier_tools/bin_flux/assets/produceFluxPlots.cpp
+++ fluxBuilder/classifier_tools/bin_flux/assets/produceFluxPlots.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <numeric>
 #include <limits>
+#include <tuple>
 
 #include "TPDF.h"
 #include "TPad.h"
@@ -21,6 +22,7 @@
 #include "TPaveLabel.h"
 #include "TEfficiency.h"
 #include "TLegendEntry.h"
+#include "TGraphErrors.h"
 
 struct energy_config {
     std::size_t n_bins;
@@ -87,10 +89,11 @@ void produceFluxPlots(
     auto energy_nbins = (int)energy_binning.size() - 1;
 
     // Find best point in energy bin
-    auto getBestPointCoordinates = [](std::shared_ptr<TGraph> gr, int steps = 50) -> std::pair<double, double> {
+    auto getBestPointCoordinates = [](std::shared_ptr<TGraph> gr, int steps = 50) -> std::tuple<double, double, double> {
         
         std::vector<std::pair<double, double>> pstack;      // Collection of tmp points
         std::vector<std::pair<double, double>> pmean;       // Collection of mean values, each one in a 2*steps points
+        std::vector<std::pair<double, double>> pstd;        // Collection of std values, each one in a 2*steps points
         std::pair<double, double> reference_point;          // Tmp reference point
 
         // Evaluate mean on std::vector<std::pair<double, double>>
@@ -101,7 +104,21 @@ void produceFluxPlots(
             return std::accumulate(diff.begin(), diff.end(), 0.)/static_cast<double>(diff.size());
         };
 
-        // Loop pver graph points
+        // Evaluate std on std::vector<std::pair<double, double>>
+        auto vstd = [&reference_point](const std::vector<std::pair<double, double>> &v) -> double {
+            std::vector<double> p;
+            for (auto& elm : v)
+                p.push_back(elm.second);
+
+            double mean {std::accumulate(std::begin(p), std::end(p), 0.0)/static_cast<double>(v.size())};
+            double accum {0};
+            std::for_each (std::begin(p), std::end(p), [&](const double val) {
+                accum += std::pow((val - mean), 2);
+            });
+            return sqrt(accum/static_cast<double>(v.size()-1));
+        };
+
+        // Loop over graph points
         for (int gr_idx=0; gr_idx<gr->GetN(); ++gr_idx) {
 
             if(!gr_idx || gr_idx%steps)
@@ -112,6 +129,8 @@ void produceFluxPlots(
             if (static_cast<int>(pstack.size()) == 2*steps) {
                 // Update mean vector
                 pmean.push_back(std::make_pair(reference_point.first, vmean(pstack)));
+                // Update std vector
+                pstd.push_back(std::make_pair(reference_point.first, vstd(pstack)));
                 // Clear stack of points
                 pstack.clear();
                 // Go back of steps
@@ -121,28 +140,35 @@ void produceFluxPlots(
         }
 
         // Use remaining points, if any
-        if (pstack.size())
+        if (pstack.size()) {
+            // Update mean vector
             pmean.push_back(std::make_pair(reference_point.first, vmean(pstack)));
+            // Update std vector
+            pstd.push_back(std::make_pair(reference_point.first, vstd(pstack)));   
+        }
 
         // Get best BDT point in range
-        auto GetBestBDT = [&gr](std::vector<std::pair<double, double>> &pmean) -> std::pair<double, double> {
+        auto GetBestBDT = [&gr](std::vector<std::pair<double, double>> &pmean, std::vector<std::pair<double, double>> &pstd) -> std::tuple<double, double, double> {
             double best_bdt {0.25};
             double min_diff {std::numeric_limits<double>::max()};
+            double low_interval_edge {-0.4};
+            double high_interval_edge {0.4};
+            double err {0};
 
-            for (auto& elm : pmean) {
-                if (elm.first > -0.4 && elm.first < 0.4) {
-                    if (elm.second < min_diff) {
-                        min_diff = elm.second;
-                        best_bdt = elm.first;
+            for (size_t idx=0; idx<pmean.size(); ++idx) {
+                if (pmean[idx].first > low_interval_edge && pmean[idx].first < high_interval_edge) {
+                    if (pmean[idx].second < min_diff) {
+                        min_diff = pmean[idx].second;
+                        best_bdt = pmean[idx].first;
+                        err = pstd[idx].second;
                     }
                 }
             }
 
-            return std::make_pair(best_bdt, gr->Eval(best_bdt));
+            return std::make_tuple(best_bdt, gr->Eval(best_bdt), err);
         };
         
-        return GetBestBDT(pmean);
-
+        return GetBestBDT(pmean, pstd);
     };
 
     // Extract fluxes from file 
@@ -194,6 +220,7 @@ void produceFluxPlots(
         auto best_point_coordinates = getBestPointCoordinates(flux_graphs_eff_corrected[bidx]);
         auto best_point_coordinates_b_sub = getBestPointCoordinates(flux_graphs_eff_corrected_b_sub[bidx]);
 
+        /*
         TMarker marker(best_point_coordinates.first, best_point_coordinates.second, 41);
         TMarker marker_b_sub(best_point_coordinates_b_sub.first, best_point_coordinates_b_sub.second, 41);
         marker.SetMarkerColor(kGreen+2);
@@ -202,7 +229,31 @@ void produceFluxPlots(
         marker_b_sub.SetMarkerColor(kOrange+2);
         marker_b_sub.SetMarkerSize(2);
         marker_b_sub.Draw("same");
-        
+        */
+
+        // Instead of TMarker I use TGraph with a single point. This permits to add errors along X axis
+        std::vector<double> bp_x_f_ec {std::get<0>(best_point_coordinates)};
+        std::vector<double> bp_y_f_ec {std::get<1>(best_point_coordinates)};
+        std::vector<double> bp_ex_f_ec {std::get<2>(best_point_coordinates)};
+        std::vector<double> bp_ey_f_ec {0.};
+
+        std::vector<double> bp_x_f_ec_b_sub {std::get<0>(best_point_coordinates_b_sub)};
+        std::vector<double> bp_y_f_ec_b_sub {std::get<1>(best_point_coordinates_b_sub)};
+        std::vector<double> bp_ex_f_ec_b_sub {std::get<2>(best_point_coordinates_b_sub)};
+        std::vector<double> bp_ey_f_ec_b_sub {0.};
+
+        TGraphErrors gr_best_point_fec(bp_ex_f_ec.size(), bp_x_f_ec.data(), bp_y_f_ec.data(), bp_ex_f_ec.data(), bp_ey_f_ec.data());
+        TGraphErrors gr_best_point_fec_b_sub(bp_ex_f_ec_b_sub.size(), bp_x_f_ec_b_sub.data(), bp_y_f_ec_b_sub.data(), bp_ex_f_ec_b_sub.data(), bp_ey_f_ec_b_sub.data());
+
+        gr_best_point_fec.SetLineWidth(2);
+        gr_best_point_fec.SetLineColor(kGreen+2);
+
+        gr_best_point_fec_b_sub.SetLineWidth(2);
+        gr_best_point_fec_b_sub.SetLineColor(kOrange+2);
+
+        gr_best_point_fec.Draw("P");
+        gr_best_point_fec_b_sub.Draw("P");
+
         gPad->SetGrid(1,1);
         if (logy) {
             gPad->SetLogy();
