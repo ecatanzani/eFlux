23fd456b66464188c7ab178e6eb713ff899efaf6
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2022-05-24 16:42:28 +0200

    new energy config file has been parsed

--- Aladin/include/energy_config.h
+++ Aladin/include/energy_config.h
@@ -7,12 +7,13 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+
 class energy_config
 {
 public:
     energy_config(const std::string working_dir);
     ~energy_config(){};
-    std::vector<float> GetEnergyBinning();
+    std::vector<double> GetEnergyBinning();
     const double GetMinEvtEnergy();
     const double GetMaxEvtEnergy();
     void PrintActiveFilters();
@@ -22,9 +23,9 @@ private:
     void get_config_info(const std::string parsed_config);
 
     std::size_t n_bins;
-    double min_event_energy = -999;
-    double max_event_energy = -999;
-    std::vector<float> energy_binning;
+    double min_event_energy {-999};
+    double max_event_energy {-999};
+    std::vector<double> energy_binning;
 };
 
 #endif
